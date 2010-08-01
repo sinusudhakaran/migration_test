a14ac58419fe2b75854437601d63f8ebde92914d
BANKLINK\scott.wilson <BANKLINK\scott.wilson>
2010-08-02 09:12:42 +1200

    Update search paths for TFS

--- Practice/Win32/bkrelink.dpr
+++ Practice/Win32/bkrelink.dpr
@@ -3,12 +3,6 @@ program bkrelink;
 uses
   Forms,
   frmResync in 'frmResync.pas' {ResynchronizeFrm},
-  FILES in 'FILES.PAS',
-  CLOBJ32 in 'CLOBJ32.PAS',
-  BKDEFS in '..\Shared\BKDEFS.PAS',
-  MONEYDEF in '..\Shared\MONEYDEF.PAS',
-  DATEDEF in '..\Shared\DATEDEF.PAS',
-  stdate in '..\..\Components\Turbopower\SysTools for BK5\stdate.pas',
   imagesfrm in 'imagesfrm.pas' {AppImages};
 
 {$R *.res}
