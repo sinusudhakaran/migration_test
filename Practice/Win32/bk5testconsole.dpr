256f3d5ff2afc34ad9a25a0a088590ee718cd1e9
BANKLINK\scott.wilson <BANKLINK\scott.wilson>
2010-08-03 09:02:38 +1200

    Test console search paths

--- Practice/Win32/bk5testconsole.dpr
+++ Practice/Win32/bk5testconsole.dpr
@@ -3,14 +3,14 @@ program bk5testconsole;
 uses
   TestFrameWork,
   TextTestRunner,
-  utWebXOffice in 'Tests\utWebXOffice.pas',
-  utMemorisationsObj in 'Tests\utMemorisationsObj.pas',
-  UTmxUtils in 'Tests\UTmxUtils.pas',
-  utPayeeObj in 'Tests\utPayeeObj.pas',
-  utMYOB in 'Tests\utMYOB.pas',
-  utGUIReportSchedule in 'Tests\utGUIReportSchedule.pas',
-  utUpgrade in 'Tests\utUpgrade.pas',
-  utFileExtensionUtils in 'Tests\utFileExtensionUtils.pas';
+  utFileExtensionUtils in '..\Tests\utFileExtensionUtils.pas',
+  utGUIReportSchedule in '..\Tests\utGUIReportSchedule.pas',
+  utMemorisationsObj in '..\Tests\utMemorisationsObj.pas',
+  UTmxUtils in '..\Tests\UTmxUtils.pas',
+  utMYOB in '..\Tests\utMYOB.pas',
+  utPayeeObj in '..\Tests\utPayeeObj.pas',
+  utUpgrade in '..\Tests\utUpgrade.pas',
+  utWebXOffice in '..\Tests\utWebXOffice.pas';
 
 {$R *.res}
 
