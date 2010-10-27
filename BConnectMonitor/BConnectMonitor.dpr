25e48f80d0e7c4994b9c0100c052bc9a1602b160
BANKLINK\Tony.Kelly <BANKLINK\Tony.Kelly>
2010-10-27 17:33:10 +1300

    BConnectMonitor (Scenario 5023)

---
+++ BConnectMonitor/BConnectMonitor.dpr
@@ -0,0 +1,260 @@
+program BConnectMonitor;   // based off BulkExtractfrm in Practice
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+  ipshttps,
+  idsmtp,
+  idmessage,
+  idemailaddress,
+  Classes,
+  Forms,
+  Contnrs,
+  Globals,
+  Winutils,
+  INISettings,
+  HyperFrm,
+  Dialogs;
+
+const
+  WebAppPath = '/bconnect/bcWebServer.dll/';
+
+type
+  TBulkExtractors = class(TObjectList)
+  private
+    procedure HTTPClientHeader(Sender: TObject; const Field, Value: string);  
+  public
+  end;
+
+var
+  HTTPClient: TipsHTTPS;
+  LServerResponseMsg : string;
+  LUsingPrimaryHost: Boolean;
+  LsKey: string;
+  PostData, FHeaders: TStringList;
+
+  FBulkExtractors : TBulkExtractors;
+
+function ExtractDetails: Integer;
+begin
+  LServerResponseMsg := FHeaders.Values['serverresponse'];
+  Result := StrToIntDef(FHeaders.Values['errorno'], 0);
+end;
+
+function GetFirewallType: TipshttpsFirewallTypes;
+begin
+  try
+    Result := TipshttpsFirewallTypes(INI_BCFirewallType);
+  except
+    Result := TipshttpsFirewallTypes(0);
+  end;
+end;
+
+function BConnectServerPath: string;
+var
+  AHost: string;
+  APort: integer;
+begin
+  if LUsingPrimaryHost then begin
+     AHost := INI_BCPrimaryHost;
+     APort := INI_BCPrimaryPort;
+  end else begin
+     AHost := INI_BCSecondaryHost;
+     APort := INI_BCSecondaryPort;
+  end;
+  if APort <> 0 then
+     Result := Format('%s%s:%d%s', [INI_BCHTTPMethod, AHost, APort, WebAppPath])
+  else
+     Result := Format('%s%s%s', [INI_BCHTTPMethod, AHost, WebAppPath]);
+end;
+
+procedure LogIn;
+var
+  ParamNum: integer;
+  AllRecipients: string;
+  BConnectIsOnline: boolean;
+  IdSMTP1: TIdSMTP;
+  Message1: TIdMessage;
+
+  function CountryAsString: string;
+  begin
+    case  AdminSystem.fdFields.fdCountry of
+    0: Result := 'NZ';
+    1: Result := 'OZ';
+    2: Result := 'UK';
+    end;
+  end;
+
+  procedure ConnectAndSendMessage;
+  begin
+    try
+      IdSMTP1.ConnectTimeout := 1000;
+      IdSMTP1.Connect;
+      IdSMTP1.Send(Message1);
+    finally
+      if idSMTP1.Connected then
+        idSMTP1.Disconnect(true);
+    end;
+  end;
+
+begin
+  if (ParamCount = 0) then
+  begin
+    ShowMessage('You must enter one or more email addresses as parameters. Program will now exit.');
+    Exit;
+  end;
+
+  BConnectIsOnline := True;
+  AllRecipients := '';
+  INI_Mail_Type := SMTP_MAIL;
+
+  try
+    IdSMTP1 := TIdSMTP.Create(Application);
+    IdSMTP1.Host := 'BANKLINK-XX1';
+    IdSMTP1.Port := 25;
+    
+    for ParamNum := 1 to ParamCount do
+      AllRecipients := ParamStr(ParamNum) + ';';
+
+    Message1 := TIdMessage.Create(Application);
+    Message1.From.Address := 'Tony.Kelly@banklink.co.nz';
+    Message1.Recipients.EMailAddresses := AllRecipients;
+
+    while true do begin // Keep running this loop until the program is terminated
+      try
+        PostData.Clear;
+        PostData.Add('user=testuser');
+        PostData.Add('pwd=testpass');
+        PostData.Add('country=NZ');
+        PostData.Add('encrypted=Y');
+        httpClient.ResetHeaders;
+        httpClient.PostData := PostData.Text;
+        httpClient.URL := BConnectServerPath + 'login';
+        httpClient.Post(httpClient.URL);
+
+        case ExtractDetails of
+          0:    begin
+                  // Connection to BConnect failed
+                  WriteLn('FAILED to connect to BConnect at ' + DateTimeToStr(Now));
+                  if BConnectIsOnline then
+                  begin
+                    // Send email (if we haven't already)
+                    Message1.Subject := 'BConnect is down';
+                    Message1.Body.Text := 'No further notifications will be sent until BConnect has been restored.';
+                    ConnectAndSendMessage;
+                    BConnectIsOnline := False;
+                  end;
+                end;
+          else  begin
+                  // Connected to BConnect successfully
+                  WriteLn('Successfully connected to BConnect at ' + DateTimeToStr(Now));
+                  if not BConnectIsOnline then
+                  begin
+                    // Send email when BConnect is back online
+                    Message1.Subject := 'BConnect is back online';
+                    Message1.Body.Text := 'No further notifications will be sent until BConnect goes down';
+                    ConnectAndSendMessage;
+                    BConnectIsOnline := True;
+                  end;
+                end;
+        end;
+        Sleep(10000); // Check BConnect status every 10 seconds
+        except
+          LUsingPrimaryHost := not LUsingPrimaryHost;
+      end;
+    end;
+
+  finally
+    Message1.Free;
+    Message1 := nil;
+    IdSMTP1.Free;
+    IdSMTP1 := nil;
+  end;
+
+
+end;
+
+procedure TBulkExtractors.HTTPClientHeader(Sender: TObject; const Field, Value: string);
+begin
+  FHeaders.Add (Format ('%s=%s', [Field, Value]));
+end;
+
+begin
+  try
+    SHORTAPPNAME := 'BankLink Practice';
+    BK5ReadINI;
+
+    FHeaders := TStringList.Create;
+    HTTPClient := TipsHTTPS.Create(nil);
+    PostData := TStringList.Create;
+    try
+      // Initialize the HTTPClient;
+
+      httpClient.OnHeader := FBulkExtractors.HTTPClientHeader;
+      httpClient.Timeout :=  INI_BCTimeout;
+      httpClient.ProxyServer := '';
+      httpClient.ProxyPort := 0;
+      httpClient.ProxyUser := '';
+      httpClient.ProxyPassword := '';
+
+      httpClient.FirewallUser := '';
+      httpClient.FirewallPassword := '';
+      httpClient.FirewallHost := '';
+      httpClient.FirewallPort := 0;
+//      httpClient.FirewallType := fwNone;
+      LUsingPrimaryHost := True;
+
+      if not (not  INI_BCCustomConfig or (INI_BCCustomConfig and INI_BCUseWinInet)) then
+         httpClient.Config('useWinInet=False')
+      else
+         httpClient.Config('useWinInet=True');
+
+      httpClient.Config (Format( 'UserAgent=BConnect/2.1 (%s %s, %s)',
+                                [ Globals.ShortAppName,
+                                  WinUtils.GetShortAppVersionStr,
+                                  WinUtils.GetWinVer]));
+
+      httpClient.Config ('SSLSecurityFlags=0x80000000');
+      httpClient.Config ('SSLEnabledProtocols=140');
+
+      if INI_BCCustomConfig then begin
+         if INI_BCUseProxy then begin
+            httpClient.ProxyServer   := INI_BCProxyHost;
+            httpClient.ProxyPort     := INI_BCProxyPort;
+            case INI_BCProxyAuthMethod of
+              1 :  begin
+                       httpClient.ProxyUser := INI_BCProxyUsername;
+                       httpClient.ProxyPassword := INI_BCProxyPassword;
+                       httpClient.OtherHeaders := Format ('Proxy-Authorization: %s' + Chr (13) + Chr (10), [httpClient.ProxyAuthorization]);
+                       httpClient.ProxyUser := '';
+                       httpClient.ProxyPassword := '';
+                   end;
+              2 :  httpClient.ProxyServer := Format ('*%s*%s', [INI_BCProxyUsername, INI_BCProxyPassword]);
+            end;
+            httpClient.ProxyUser     := INI_BCProxyUsername;
+            httpClient.ProxyPassword := INI_BCProxyPassword;
+         end;
+
+        if INI_BCUseFirewall then begin
+           httpClient.FirewallHost := INI_BCFirewallHost;
+           httpClient.FirewallPort := INI_BCFirewallPort;
+           httpClient.FirewallType := GetFirewallType;
+           if INI_BCFirewallUseAuth then begin
+              httpClient.FirewallUser     := INI_BCFirewallUsername;
+              httpClient.FirewallPassword := INI_BCFirewallPassword;
+          end;
+        end;
+      end;
+      Login; // Try to logon
+    finally
+      HTTPClient.Free;
+      Postdata.Free;
+    end;
+  except
+    on E:Exception do
+      Writeln(E.Classname, ': ', E.Message);
+  end;
+
+end.
+
