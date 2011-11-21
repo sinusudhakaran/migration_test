f824ec6be4229de28b5d80a67b7929fad3b69932
BANKLINK\scott.wilson <BANKLINK\scott.wilson>
2011-11-21 16:40:21 +1300

    Added Practice BLO registration form

---
+++ Practice/Win32/RequestRegFrm.dfm
@@ -0,0 +1,176 @@
+object RequestregForm: TRequestregForm
+  Left = 0
+  Top = 0
+  BorderStyle = bsDialog
+  Caption = 'Request BankLink Online Registration'
+  ClientHeight = 403
+  ClientWidth = 549
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poScreenCenter
+  OnCloseQuery = FormCloseQuery
+  OnShow = FormShow
+  DesignSize = (
+    549
+    403)
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Bevel10: TBevel
+    Left = 128
+    Top = 17
+    Width = 401
+    Height = 8
+    Shape = bsTopLine
+  end
+  object imgInfo: TImage
+    Left = 16
+    Top = 270
+    Width = 49
+    Height = 66
+    Transparent = True
+  end
+  object lblPleaseAllow: TLabel
+    Left = 80
+    Top = 270
+    Width = 449
+    Height = 23
+    AutoSize = False
+    Caption = 
+      'Please allow three working days for your application to be proce' +
+      'ssed'
+  end
+  object lblInfo: TLabel
+    Left = 80
+    Top = 302
+    Width = 433
+    Height = 50
+    AutoSize = False
+    Caption = 
+      'When your secure area has been created, a member of the BankLink' +
+      ' Support Team will contact the above BankLink Online Administrat' +
+      'or to provide assistance with using the BankLink Online service'
+    WordWrap = True
+  end
+  object lblPracticeName: TLabel
+    Left = 16
+    Top = 40
+    Width = 27
+    Height = 13
+    Caption = 'Name'
+  end
+  object lblSecureCode: TLabel
+    Left = 16
+    Top = 75
+    Width = 105
+    Height = 13
+    Caption = 'BankLink Secure Code'
+  end
+  object Label5: TLabel
+    Left = 16
+    Top = 115
+    Width = 187
+    Height = 13
+    Caption = 'Administrator Details (Primary Contact)'
+  end
+  object lblAdminName: TLabel
+    Left = 16
+    Top = 152
+    Width = 27
+    Height = 13
+    Caption = 'Name'
+  end
+  object lblPh: TLabel
+    Left = 16
+    Top = 192
+    Width = 70
+    Height = 13
+    Caption = 'Phone Number'
+  end
+  object lblEmail: TLabel
+    Left = 16
+    Top = 232
+    Width = 66
+    Height = 13
+    Caption = 'Email Address'
+  end
+  object Bevel1: TBevel
+    Left = 264
+    Top = 123
+    Width = 265
+    Height = 8
+    Shape = bsTopLine
+  end
+  object Label9: TLabel
+    Left = 16
+    Top = 8
+    Width = 73
+    Height = 13
+    Caption = 'Practice Details'
+  end
+  object btnSubmit: TButton
+    Left = 365
+    Top = 370
+    Width = 75
+    Height = 25
+    Anchors = [akRight, akBottom]
+    Caption = '&Submit'
+    Default = True
+    ModalResult = 1
+    TabOrder = 0
+    OnClick = btnSubmitClick
+  end
+  object btnCancel: TButton
+    Left = 454
+    Top = 370
+    Width = 75
+    Height = 25
+    Anchors = [akRight, akBottom]
+    Cancel = True
+    Caption = 'Cancel'
+    ModalResult = 2
+    TabOrder = 1
+  end
+  object edtPracticeName: TEdit
+    Left = 160
+    Top = 37
+    Width = 369
+    Height = 21
+    TabOrder = 2
+  end
+  object edtSecureCode: TEdit
+    Left = 160
+    Top = 72
+    Width = 369
+    Height = 21
+    TabOrder = 3
+  end
+  object edtPh: TEdit
+    Left = 160
+    Top = 192
+    Width = 369
+    Height = 21
+    TabOrder = 4
+  end
+  object edtEmail: TEdit
+    Left = 160
+    Top = 227
+    Width = 369
+    Height = 21
+    TabOrder = 5
+  end
+  object cbAdminName: TComboBox
+    Left = 160
+    Top = 149
+    Width = 369
+    Height = 21
+    Style = csDropDownList
+    ItemHeight = 0
+    TabOrder = 6
+    OnChange = cbAdminNameChange
+  end
+end
---
+++ Practice/Win32/RequestRegFrm.pas
@@ -0,0 +1,177 @@
+unit RequestRegFrm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ExtCtrls, StdCtrls, ImagesFrm, OSFont;
+
+type
+  TRequestregForm = class(TForm)
+    btnSubmit: TButton;
+    btnCancel: TButton;
+    Bevel10: TBevel;
+    imgInfo: TImage;
+    lblPleaseAllow: TLabel;
+    lblInfo: TLabel;
+    lblPracticeName: TLabel;
+    lblSecureCode: TLabel;
+    Label5: TLabel;
+    lblAdminName: TLabel;
+    lblPh: TLabel;
+    lblEmail: TLabel;
+    Bevel1: TBevel;
+    edtPracticeName: TEdit;
+    edtSecureCode: TEdit;
+    edtPh: TEdit;
+    edtEmail: TEdit;
+    cbAdminName: TComboBox;
+    Label9: TLabel;
+    procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+    procedure cbAdminNameChange(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure btnSubmitClick(Sender: TObject);
+  private
+    { Private declarations }
+    function VerifyForm: Boolean;
+    procedure SetupForm;
+  public
+    { Public declarations }
+  end;
+
+  function RequestBankLinkOnlineregistration: Boolean;
+
+implementation
+
+uses
+  Globals,
+  SYDEFS,
+  WarningMoreFrm,
+  MailFrm;
+
+{$R *.dfm}
+
+function RequestBankLinkOnlineregistration: Boolean;
+var
+  RequestRegForm: TRequestregForm;
+begin
+  RequestRegForm := TRequestregForm.Create(Application.MainForm);
+  try
+    RequestRegForm.SetupForm;
+    Result := (RequestRegForm.ShowModal = mrOK);
+  finally
+    RequestRegForm.Free;
+  end;
+end;
+
+procedure TRequestregForm.btnSubmitClick(Sender: TObject);
+const
+  ONE_LINE = #10;
+  TWO_LINES = #10#10;
+var
+  Msg: string;
+begin
+  Msg := Format('Please register this practice on BankLink Online%s' +
+                'Practice Name: %s%s' +
+                'Practice Code: %s%s' +
+                'The BankLink Online Administrator (Primary Contact) for the practice%s' +
+                'Name: %s%s' +
+                'Phone Number: %s%s' +
+                'Email Address: %s%s',
+                [TWO_LINES,
+                 edtPracticeName.Text, ONE_LINE,
+                 edtSecureCode.Text, TWO_LINES, TWO_LINES,
+                 cbAdminName.Items[cbAdminName.ItemIndex], ONE_LINE,
+                 edtPh.Text, ONE_LINE,
+                 edtEmail.Text, TWO_LINES]);
+
+  SendMailTo('BankLink Online Registration', GetSupportEmailAddress,
+             'BankLink Online Registration', Msg);
+end;
+
+procedure TRequestregForm.cbAdminNameChange(Sender: TObject);
+begin
+  //Practice user selected
+  edtPh.Text := '';
+  edtEmail.Text := '';
+  if (cbAdminName.ItemIndex > 0) then begin
+    if (Trim(edtPh.Text) = '') and (Trim(edtEmail.Text) = '') then begin
+      edtPh.Text := pUser_Rec(cbAdminName.Items.Objects[cbAdminName.ItemIndex]).usDirect_Dial;
+      edtEmail.Text := pUser_Rec(cbAdminName.Items.Objects[cbAdminName.ItemIndex]).usEMail_Address;
+    end;
+  end;
+end;
+
+procedure TRequestregForm.FormCloseQuery(Sender: TObject;
+  var CanClose: Boolean);
+begin
+  CanClose := True;
+  if (ModalResult = mrOk) then
+    CanClose := VerifyForm;
+end;
+
+procedure TRequestregForm.FormShow(Sender: TObject);
+begin
+  if edtPracticeName.CanFocus then
+    edtPracticeName.SetFocus;
+end;
+
+procedure TRequestregForm.SetupForm;
+var
+  i: integer;
+  User: pUser_Rec;
+begin
+  //Image
+  ImgInfo.Picture := AppImages.InfoBmp.Picture;
+  //Practice details
+  if Trim(AdminSystem.fdFields.fdPractice_Name_for_Reports) <> '' then begin
+    edtPracticeName.Text := AdminSystem.fdFields.fdPractice_Name_for_Reports;
+    lblPracticeName.Enabled := False;
+  end;
+  edtSecureCode.Text := AdminSystem.fdFields.fdBankLink_Code;
+  edtSecureCode.Enabled := False;
+  //Admin details
+  cbAdminName.Items.Add('');  //Default to blank
+  for i := AdminSystem.fdSystem_User_List.First to AdminSystem.fdSystem_User_List.Last do begin
+    User := AdminSystem.fdSystem_User_List.User_At(i);
+    if User.usSystem_Access then
+      cbAdminName.Items.AddObject(User.usName, TObject(User));
+  end;
+  cbAdminName.ItemIndex := 0;
+end;
+
+function TRequestregForm.VerifyForm: Boolean;
+begin
+  Result := False;
+
+  //Practice name
+  if edtPracticeName.Enabled and (Trim(edtPracticeName.Text) = '') then begin
+    if edtPracticeName.CanFocus then
+      edtPracticeName.SetFocus;
+    HelpfulWarningMsg('A valid practice name is required for this request. '  +
+                      'Please try again.',0);
+    Exit;
+  end;
+
+  //Admin name
+  if (cbAdminName.ItemIndex = 0) then begin
+    if cbAdminName.CanFocus then
+      cbAdminName.SetFocus;
+    HelpfulWarningMsg('A valid administrator name is required for this request. '  +
+                      'Please try again.',0);
+    Exit;
+  end;
+
+  //Email
+  if edtEmail.Enabled and (Trim(edtEmail.Text) = '') then begin
+    if edtEmail.CanFocus then
+      edtEmail.SetFocus;
+    HelpfulWarningMsg('A valid administrator email address is required for this request. '  +
+                      'Please try again.',0);
+    Exit;
+  end;
+
+  Result := True;
+end;
+
+end.
