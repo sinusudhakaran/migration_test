4a81f1df4fd51382a873584196a4198d2b70c764
BANKLINK\ajoosten <BANKLINK\ajoosten>
2012-02-14 12:59:39 +1300

    EXL updates

--- Common/DBGen/XMLWrite.pas
+++ Common/DBGen/XMLWrite.pas
@@ -26,8 +26,10 @@ uses
       fCode      : ShortString;
       fReadCode  : String;
       fWriteCode : string;
-      FCClass : string;
+      fCProperty : string;
+      fCArrayClass : string;
    end;
+
 
    CodeList = Class(TObjectList)
      function FindCode(code,  Name: string):tCodeBlock;
@@ -105,6 +107,7 @@ var
    WritePart: TStringList;
 
    CPart: TStringList;
+   CArrayClasses: TStringList;
 
    procedure WriteLines(FieldCode: tCodeBlock; Name: string);
       function MakeCode(value:string): string ;
@@ -121,8 +124,13 @@ var
          cPart.Add(#09#09'/// <summary>');
          cPart.Add(format(#09#09'/// %s property',[MakeSaveName(Name)]));
          cPart.Add(#09#09'/// </summary>');
-         cPart.Add(MakeCode(FieldCode.FCClass) );
+         cPart.Add(MakeCode(FieldCode.fCProperty) );
          cPart.Add('');
+
+         if FieldCode.fCArrayClass > '' then begin
+            CArrayClasses.Add(MakeCode(FieldCode.fCArrayClass));
+         end;
+
    end;
 
 
@@ -134,6 +142,7 @@ begin
    CPart:= TStringList.Create;
    InterfacePart := TStringList.Create;
    ImplementationPart := TStringList.Create;
+   CArrayClasses := TStringList.Create;
    // Make the Pas File
 
 
@@ -162,12 +171,17 @@ begin
             CPart.Clear;
             ReadPart.Clear;
             WritePart.Clear;
+            CArrayClasses.Clear;
 
             Assign( WriteCFile, SysName + MakeCleanName(Name) + '.cs');
             Rewrite( WriteCFile );
          end  else  If LineType='F' then  Begin
             // Individual  Fields
             FieldName := GetAField(2);
+            
+            if Sametext(Fieldname, 'Audit Record ID') then
+               Continue;
+
             FieldCode := GetAField(3);
             CodeBlock := FieldTypes.FindCode( FieldCode, prefix + MakeName(FieldName) );
             B1 := GetBField( 4 );
@@ -199,8 +213,8 @@ begin
            ImplementationPart.Add('');
 
            ImplementationPart.Add(Format('function %sHelper.WriteRecToNode(var Node: IXMLNode): IXMLNode;',[MakeRecName(Name)]));
-           ImplementationPart.Add('var PNode, CNode: IXMLNode;');
-           ImplementationPart.Add(#9'I: Integer;');
+           ImplementationPart.Add('var PNode, CNode, CCNode: IXMLNode;');
+           ImplementationPart.Add(#9'I, J, M: Integer;');
            ImplementationPart.Add('begin');
            ImplementationPart.Add(#9'Result := Node.AddChild(GetXMLNodeName);');
 
@@ -209,6 +223,7 @@ begin
 
            ImplementationPart.Add('');
            ImplementationPart.Add(#9'CNode := nil;');
+           ImplementationPart.Add(#9'CCNode := nil;');
            ImplementationPart.Add(#9'PNode := nil;');
            ImplementationPart.Add('end;');
 
@@ -216,8 +231,8 @@ begin
            ImplementationPart.Add('');
 
            ImplementationPart.Add( Format('function %sHelper.ReadRecFromNode(Node :IXMLNode): IXMLNode;',[MakeRecName(Name)]));
-           ImplementationPart.Add('var PNode, CNode: IXMLNode;');
-           ImplementationPart.Add(#9'I: Integer;');
+           ImplementationPart.Add('var PNode, CNode, CCNode: IXMLNode;');
+           ImplementationPart.Add(#9'I, J: Integer;');
            ImplementationPart.Add('begin');
 
            ImplementationPart.Add(#9'if Node.NodeName = GetXMLNodeName then Result := Node');
@@ -225,12 +240,19 @@ begin
            ImplementationPart.Add(#9'Result := Node.ChildNodes.FindNode(GetXMLNodeName);');
            ImplementationPart.Add(#9'if not assigned(Result) then exit;');
 
+           ImplementationPart.Add(#9'try');
+
            for I := 0 to WritePart.Count - 1 do
                ImplementationPart.Add(#9 + ReadPart[I]);
 
+           ImplementationPart.Add(#9'except');
+           ImplementationPart.Add(Format(#9#9'on E: Exception do ReRaise (E, ''Reading %s'');',[MakeRecName(Name)]));
+           ImplementationPart.Add(#9'end;');
            ImplementationPart.Add('');
-         
+
+
            ImplementationPart.Add(#9'CNode := nil;');
+           ImplementationPart.Add(#9'CCNode := nil;');
            ImplementationPart.Add(#9'PNode := nil;');
            ImplementationPart.Add('end;');
 
@@ -258,7 +280,7 @@ begin
             WriteLn(WriteCFile,'{');
 
             WriteLn(WriteCFile,#09'/// <summary>');
-            WriteLn(WriteCFile,Format(#09'///%s - %s class',[SysName,MakeCleanName(Name)]));
+            WriteLn(WriteCFile,Format(#09'/// %s - %s class',[SysName,MakeCleanName(Name)]));
             WriteLn(WriteCFile,#09'/// </summary>');
             WriteLn(WriteCFile,format(#09'public partial class %s%s',[SysName,MakeCleanName(Name)]));
             WriteLn(WriteCFile,#09'{');
@@ -269,19 +291,25 @@ begin
 
 
             WriteLn(WriteCFile,'');
-            WriteLn(WriteCFile,#09'}');
-            WriteLn(WriteCFile,'}');
+            WriteLn(WriteCFile,#09'}'); // Class end
 
             WriteLn(WriteCFile,'');
             WriteLn(WriteCFile,'');
-            WriteLn(WriteCFile,'');
-            WriteLn(WriteCFile,'');
-            WriteLn(WriteCFile,'');
-            WriteLn(WriteCFile,'');
+
+            for I := 0 to CArrayClasses.Count - 1 do begin
+               WriteLn(WriteCFile, CArrayClasses[I]);
+               WriteLn(WriteCFile,'');
+               WriteLn(WriteCFile,'');
+            end;
+
+            WriteLn(WriteCFile,'}');  // Name space end
+
             WriteLn(WriteCFile,'');
             WriteLn(WriteCFile,'');
 
 
+
+           
             Close(WriteCFile);
          end;
       end;
@@ -313,9 +341,18 @@ begin
    WriteLn(WritePFile,'// ----------------------------------------------------------------------------');
    WriteLn(WritePFile,'implementation');
    WriteLn(WritePFile,'uses');
-   WriteLn(WritePFile,#09'WebUtils;');
+   WriteLn(WritePFile,#09'WebUtils,');
+   WriteLn(WritePFile,#09'SysUtils;');
    WriteLn(WritePFile,'');
    WriteLn(WritePFile,'');
+
+   // Add Private functions..
+   WriteLn(WritePFile,'procedure ReRaise(E: Exception; Doing: string);');
+   WriteLn(WritePFile,'begin');
+   WriteLn(WritePFile,#9'raise exception.Create( format(''Error : %s While : %s'',[E.Message, Doing]));');
+   WriteLn(WritePFile,'end;');
+   WriteLn(WritePFile,'');
+
 
     // Add The Implementation
    for I := 0 to ImplementationPart.Count - 1 do
@@ -334,6 +371,7 @@ begin
    FreeAndNil(CPart);
    FreeAndNil(InterfacePart);
    FreeAndNil(ImplementationPart);
+   FreeAndNil(CArrayClasses);
 end;
 
 
@@ -372,31 +410,112 @@ const
    NulbaseRead =  '%s := Get%sElement(Result, ''%s'');';
 
    ArrayRead =  'GetArray(Result, ''%s'', %s);';
+   // since the string arrays are passed for specific string lengths... the above does not seem to work
    SArrayRead =
-    #13#10#09'PNode := Node.ChildNodes.FindNode(''%s'');'#13#10 +
-    #9'if Assigned(PNode) then begin;'#13#10 +
-    #9#9'I := Low(%s);'#13#10 +
-    #9#9'CNode := PNode.ChildNodes.FindNode(''%s'');'#13#10 +
-    #9#9'while assigned(CNode) and (I <= High(%s)) do begin'#13#10 +
+    #13#10#09'PNode := Result.ChildNodes.FindNode(''%s'');'#13#10 +
+    #9'if Assigned(PNode) then'#13#10 +
+    #9#9'CNode := PNode.ChildNodes.FindNode(''%s'')'#13#10 +
+    #9'else'#13#10 +
+    #9#9'CNode := Nil;'#13#10 +
+    #9'for I := low(%s) to High(%s) do begin'#13#10 +
+    #9#9'if Assigned(CNode) then begin'#13#10 +
     #9#9#9'%s[I] := CNode.Nodevalue;'#13#10 +
-    #9#9#9'Cnode := CNode.NextSibling;'#13#10 +
-    #9#9'end;'#13#10 +
+    #9#9#9'CNode := CNode.NextSibling;'#13#10 +
+    #9#9'end else'#13#10 +
+    #9#9#9'%s[I] := '''''#13#10 +
     #9'end;'#13#10;
 
+
+   // Similar problem with Multy dimentional arrays..
+    MArrayRead =
+    #9'PNode := Result.ChildNodes.FindNode(''%s'');'#13#10 +
+    #9'if Assigned(PNode) then begin'#13#10 +
+    #9#9'PNode := PNode.ChildNodes.FindNode(''%s'');'#13#10 +
+    #9'end;'#13#10 +
+    #9'for I := low(%s) to High(%s) do begin'#13#10 +
+    #9#9'if Assigned(PNode) then begin'#13#10 +
+    #9#9#9'CNode := PNode.ChildNodes.FindNode(''%s'');'#13#10 +
+    #9#9#9'if Assigned(CNode) then'#13#10 +
+    #9#9#9#9'CCNode := CNode.ChildNodes.FindNode(''%s'')'#13#10 +
+    #9#9#9'else CCNode := Nil;'#13#10 +
+    #9#9'end else'#13#10 +
+    #9#9#9'CNode := Nil;'#13#10 +
+    #9#9'for J := Low(%s[I]) to High(%s[I]) do begin'#13#10 +
+    #9#9#9'if Assigned(CCNode) then begin'#13#10 +
+    #9#9#9#9'%s[I,J] := CCNode.Nodevalue;'#13#10 +
+    #9#9#9#9'CCNode := CCNode.NextSibling;'#13#10 +
+    #9#9#9'end else'#13#10 +
+    #9#9#9#9'%s[I,J] := 0;'#13#10 +
+    #9#9'end;'#13#10 +
+    #9#9#9'if Assigned(PNode) then'#13#10 +
+    #9#9#9#9'PNode := PNode.NextSibling;'#13#10 +
+    #9'end;';
 
    baseWrite =  'Set%sAttr(Result, ''%s'', %s);';
    NulbaseWrite =  'Set%sElement(Result, ''%s'', %s);';
 
    ArrayWrite =  'SetArray(Result, ''%s'', %s);';
-   SArrayWrite = 'PNode := Result.AddChild(''%s'');'#13#10#09'for I := Low(%s) to High(%s) do begin' +
-     #13#10#9#9'CNode := PNode.AddChild(''%s'');'#13#10#09#09'CNode.NodeValue := %s[I];'#13#10#09'end;';
+   // and the string version
+   SArrayWrite =
+   'PNode := Result.AddChild(''%s'');'#13#10 +
+   #9'M :=  Low(%s);'#13#10 +
+   #9'for I := Low(%s) to High(%s) do'#13#10 +
+   #9#9'if  %s[I] > '''' then'#13#10 +
+   #9#9#9'M := I;'#13#10 +
+   #9'for I := Low(%s) to M do begin'#13#10 +
+   #9#9'CNode := PNode.AddChild(''%s'');'#13#10 +
+   #9#9'CNode.NodeValue := %s[I];'#13#10 +
+   #9'end;';
+
+   // And the multi array
+   MArrayWrite =
+   'PNode := result.AddChild(''%s'');'#13#10  +
+   #9'M :=  Low(%s);'#13#10 +
+   #9'for I := Low(%s) to High(%s) do'#13#10 +
+   #9#9'for J := Low(%s[I]) to High(%s[I]) do'#13#10 +
+   #9#9#9'if %s[I,J] > 0 then begin'#13#10 +
+   #9#9#9#9'M := I;'#13#10 +
+   #9#9#9#9'Break;'#13#10 +
+   #9#9#9'end;'#13#10 +
+   #9'for I := low(%s) to M do begin'#13#10 +
+   #9#9'CNode := PNode.AddChild(''%s'');'#13#10 +
+   #9#9'CNode := CNode.AddChild(''%s'');'#13#10 +
+   #9#9'for J := Low(%s[I]) to High(%s[I]) do begin'#13#10 +
+   #9#9#9'CCNode := CNode.AddChild(''%s'');'#13#10 +
+   #9#9#9'CCNode.NodeValue := (%s[I,J]);'#13#10 +
+   #9#9'end;'#13#10 +
+   #9'end;';
 
 
    // C# Fields
-   baseField = #09#09'[XmlAttribute("%s", DataType = "%s")]'#13#10#09#09'public %s %s { get; set; }';
-   NulbaseField = #09#09'[XmlElement("%s", DataType = "%s")]'#13#10#09#09'public %s? %s { get; set; }';
-   ArrayField = #09#09'[XmlArray("%s"),XmlArrayItem("%s", DataType = "%s")]'#13#10#09#09'public %s[] %s { get; set; }';
-   MArrayField = #09#09'[XmlArray("%s"),XmlArrayItem("%s", DataType = "%s")]'#13#10#09#09'public %s[,] %s { get; set; }';
+   baseField =
+   #09#09'[XmlAttribute("%s", DataType = "%s")]'#13#10 +
+   #09#09'public %s %s { get; set; }'#13#10;
+
+   NulbaseField =
+   #9#9'[XmlElement("%s", DataType = "%s")]'#13#10 +
+   #9#9'public %s? %s { get; set; }';
+
+   ArrayField =
+   #09#09'[XmlArray("%s"),XmlArrayItem("%s", DataType = "%s")]'#13#10 +
+   #09#09'public %s[] %s { get; set; }';
+
+   MArrayField =
+   #09#09'[XmlArray("%s"),XmlArrayItem("%s")]'#13#10 +
+   #09#09'public %s[] %s { get; set; }';
+
+   ArrayClass =
+   #9'/// <summary>'#13#10 +
+	 #9'/// %s Array class'#13#10 +
+	 #9'/// </summary>'#13#10 +
+   #9'public class %s'#13#10 +
+   #9'{'#13#10 +
+   #9#9'/// <summary>'#13#10 +
+   #9#9'/// Name property'#13#10 +
+	 #9#9'/// </summary>'#13#10 +
+   #9#9'[XmlArray("%s"),XmlArrayItem("%s", DataType = "%s")]'#13#10 +
+   #9#9'public %s[] %s { get; set; }'#13#10 +
+   #9'}'#13#10;
 
 var F : tCodeBlock;
 begin
@@ -406,14 +525,14 @@ begin
     F.fCode := 'S';
     F.fReadCode  := format(baseRead,[make, 'Text', save]);
     F.fWriteCode := format(baseWrite,['Text',save, Make]);
-    F.FCClass  :=  format(baseField,[Clean,'string','String',save]);
+    F.fCProperty  :=  format(baseField,[Clean,'string','String',save]);
     FieldTypes.Add(F);
 
     F := tCodeBlock.Create;
     F.fCode := 'BS';
-    F.fReadCode  := format(SArrayRead,[Plural,Make,Single,Make, Make]);
-    F.fWriteCode := format(SArrayWrite,[Plural, make, make, Single, Make]);
-    F.FCClass  :=  format(ArrayField,[Plural,Single,'string','String',save]);
+    F.fReadCode  := format(SArrayRead,[Plural,Single,Make, Make, make, Make]);
+    F.fWriteCode := format(SArrayWrite,[Plural, make, make, make,make,make, Single, Make]);
+    F.fCProperty  :=  format(ArrayField,[Plural,Single,'string','String',save]);
     FieldTypes.Add(F);
 
 
@@ -422,7 +541,7 @@ begin
     F.fCode := 'AS';
     F.fReadCode  := format(baseRead,[make, 'Text', save]);
     F.fWriteCode := format(baseWrite,['Text',save, Make]);
-    F.FCClass  :=  format(baseField,[Clean,'string','String',save]);
+    F.fCProperty  :=  format(baseField,[Clean,'string','String',save]);
     FieldTypes.Add(F);
 
     F := tCodeBlock.Create;
@@ -435,7 +554,7 @@ begin
     { Do it as }
     F.fReadCode  := format(baseRead,[make, 'Int', save]);
     F.fWriteCode := format(baseWrite,['Int',save, Make]);
-    F.FCClass  :=  format(baseField,[save,'int','Int32',save]);
+    F.fCProperty  :=  format(baseField,[save,'int','Int32',save]);
     {}
     FieldTypes.Add(F);
 
@@ -443,21 +562,21 @@ begin
     F.fCode := 'L';
     F.fReadCode  := format(baseRead,[make, 'Int', save]);
     F.fWriteCode := format(baseWrite,['Int',save, Make]);
-    F.FCClass  :=  format(baseField,[save,'int','Int32',save]);
+    F.fCProperty  :=  format(baseField,[save,'int','Int32',save]);
     FieldTypes.Add(F);
 
     F := tCodeBlock.Create;
     F.fCode := 'BL';
-    F.fReadCode  := format(ArrayRead,[Save, Make]);
+    F.fReadCode  := format(ArrayRead,[Plural, Make]);
     F.fWriteCode := format(ArrayWrite,[Plural, Make]);
-    F.FCClass  :=  format(ArrayField,[Plural,Single,'int','Int32',save]);
+    F.fCProperty  :=  format(ArrayField,[Plural,Single,'int','Int32',save]);
     FieldTypes.Add(F);
 
     F := tCodeBlock.Create;
     F.fCode := 'BL0';
-    F.fReadCode  := format(ArrayRead,[Save, Make]);
+    F.fReadCode  := format(ArrayRead,[Plural, Make]);
     F.fWriteCode := format(ArrayWrite,[Plural, Make]);
-    F.FCClass  :=  format(ArrayField,[Plural,Single,'int','Int32',save]);
+    F.fCProperty  :=  format(ArrayField,[Plural,Single,'int','Int32',save]);
     FieldTypes.Add(F);
 
 
@@ -465,7 +584,7 @@ begin
     F.fCode := 'I';
     F.fReadCode  := format(baseRead,[make, 'Int', save]);
     F.fWriteCode := format(baseWrite,['Int',save, Make]);
-    F.FCClass  :=  format(baseField,[save,'int','Int32',save]);
+    F.fCProperty  :=  format(baseField,[save,'int','Int32',save]);
     FieldTypes.Add(F);
 
 
@@ -473,7 +592,7 @@ begin
     F.fCode := 'BI';
     F.fReadCode  := format(ArrayRead,[Save, Make]);
     F.fWriteCode := format(ArrayWrite,[Plural, Make]);
-    F.FCClass  :=  format(ArrayField,[Plural,Single,'int','Int32',save]);
+    F.fCProperty  :=  format(ArrayField,[Plural,Single,'int','Int32',save]);
     FieldTypes.Add(F);
 
 
@@ -482,45 +601,52 @@ begin
     F.fCode := 'Y';
     F.fReadCode  := format(baseRead,[make, 'Bool', save]);
     F.fWriteCode := format(baseWrite,['Bool',save, Make]);
-    F.FCClass  :=  format(baseField,[save,'boolean','bool',save]);
+    F.fCProperty  :=  format(baseField,[save,'boolean','bool',save]);
     FieldTypes.Add(F);
 
     F := tCodeBlock.Create;
     F.fCode := 'BY';
     F.fReadCode  := format(ArrayRead,[Save, Make]);
     F.fWriteCode := format(ArrayWrite,[Plural, Make]);
-    F.FCClass  :=  format(ArrayField,[Plural,Single,'boolean','bool',save]);
+    F.fCProperty  :=  format(ArrayField,[Plural,Single,'boolean','bool',save]);
+    FieldTypes.Add(F);
+
+    F := tCodeBlock.Create;
+    F.fCode := 'BY0';
+    F.fReadCode  := format(ArrayRead,[Plural, Make]);
+    F.fWriteCode := format(ArrayWrite,[Plural, Make]);
+    F.fCProperty  :=  format(ArrayField,[Plural,Single,'boolean','bool',save]);
     FieldTypes.Add(F);
 
     F := tCodeBlock.Create;
     F.fCode := 'B';
     F.fReadCode  := format(baseRead,[make, 'Int', save]);
     F.fWriteCode := format(baseWrite,['Int',save, Make]);
-    F.FCClass  :=  format(baseField,[save,'unsignedByte','byte',save]);
+    F.fCProperty  :=  format(baseField,[save,'unsignedByte','byte',save]);
     FieldTypes.Add(F);
 
     
     F := tCodeBlock.Create;
     F.fCode := 'BB0';
-    F.fReadCode  := format(ArrayRead,[Save, Make]);
+    F.fReadCode  := format(ArrayRead,[Plural, Make]);
     F.fWriteCode := format(ArrayWrite,[Plural, Make]);
-    F.FCClass  :=  format(ArrayField,[Plural,Single,'unsignedByte','byte',save]);
+    F.fCProperty  :=  format(ArrayField,[Plural,Single,'unsignedByte','byte',save]);
     FieldTypes.Add(F);
 
     F := tCodeBlock.Create;
     F.fCode := 'BB';
-    F.fReadCode  := format(ArrayRead,[Save, Make]);
+    F.fReadCode  := format(ArrayRead,[Plural, Make]);
     F.fWriteCode := format(ArrayWrite,[Plural, Make]);
-    F.FCClass  :=  format(ArrayField,[Plural,Single,'unsignedByte','byte',save]);
+    F.fCProperty  :=  format(ArrayField,[Plural,Single,'unsignedByte','byte',save]);
     FieldTypes.Add(F);
 
 
     F := tCodeBlock.Create;
     F.fCode := '$';
-    F.fReadCode  := format(NulbaseRead,[make, 'Money', save]);
-    F.fWriteCode := format(NulbaseWrite,['Money',save, Make]);
+    F.fReadCode  := format(baseRead,[make, 'Int64', save]);
+    F.fWriteCode := format(baseWrite,['Int64',save, Make]);
     //F.FCClass  :=  format(NulbaseField,[save,'decimal','Decimal',save]);
-    F.FCClass  :=  format(baseField,[save,'long','Int64',save]);
+    F.fCProperty  :=  format(baseField,[save,'long','Int64',save]);
     FieldTypes.Add(F);
 
     F := tCodeBlock.Create;
@@ -528,15 +654,24 @@ begin
     F.fReadCode  := format(ArrayRead,[Save, Make]);
     F.fWriteCode := format(ArrayWrite,[Plural, Make]);
     //F.FCClass  :=  format(arrayField,[Plural,Single,'decimal','Decimal',save]);
-    F.FCClass  :=  format(arrayField,[Plural,Single,'long','Int64',save]);
+    F.fCProperty  :=  format(arrayField,[Plural,Single,'long','Int64',save]);
+    FieldTypes.Add(F);
+
+    F := tCodeBlock.Create;
+    F.fCode := 'BB$';
+    F.fReadCode  := format(MArrayRead, [Plural, Plural, Make, Make,Plural,Single, Make,Make,Make, Make]);
+    F.fWriteCode := format(MArrayWrite,[Plural,Make,Make,Make,Make,Make,Make,Make,Plural,Plural,Make, Make,Single, Make]);
+    //F.FCClass  :=  format(arrayField,[Plural,Single,'decimal','Decimal',save]);
+    F.fCProperty  :=  format(MarrayField,[Plural,Plural,Plural,save]);
+    F.fCArrayClass :=  format(ArrayClass,[Plural,Plural,Plural,Single,'long','Int64',Single,save]);
     FieldTypes.Add(F);
 
     F := tCodeBlock.Create;
     F.fCode := 'B$0';
-    F.fReadCode  := format(ArrayRead,[Save, Make]);
+    F.fReadCode  := format(ArrayRead,[Plural, Make]);
     F.fWriteCode := format(ArrayWrite,[Plural, Make]);
     //F.FCClass  :=  format(arrayField,[Plural,Single,'decimal','Decimal',save]);
-    F.FCClass  :=  format(arrayField,[Plural,Single,'long','Int64',save]);
+    F.fCProperty  :=  format(arrayField,[Plural,Single,'long','Int64',save]);
     FieldTypes.Add(F);
 
     {
@@ -549,6 +684,8 @@ begin
     }
 
 end;
+
+
 
 Initialization
    FieldTypes := CodeList.Create(True);
