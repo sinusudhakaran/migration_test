1b9f769ce3ae575d939518b14e220c9340532dea
BANKLINK\Kerry.Convery <BANKLINK\Kerry.Convery>
2012-08-09 10:47:28 +1200

    New contra code entry forms

---
+++ Practice/Win32/ContraCodeEntryfrm.dfm
@@ -0,0 +1,83 @@
+object frmContraCodeEntry: TfrmContraCodeEntry
+  Left = 0
+  Top = 0
+  BorderStyle = bsDialog
+  Caption = 'Enter Bank Account Contra Code'
+  ClientHeight = 176
+  ClientWidth = 538
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poMainFormCenter
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 20
+    Top = 18
+    Width = 409
+    Height = 33
+    AutoSize = False
+    Caption = 
+      'BankLink Practice needs to know the account code in your client'#39 +
+      's chart for the bank account ABC LIMITED.'
+    WordWrap = True
+  end
+  object Label2: TLabel
+    Left = 20
+    Top = 88
+    Width = 93
+    Height = 13
+    Caption = '&Bank Account Code'
+    FocusControl = edtBankAccountCode
+  end
+  object sbtnChart: TSpeedButton
+    Left = 278
+    Top = 85
+    Width = 24
+    Height = 22
+    Flat = True
+    OnClick = sbtnChartClick
+  end
+  object lblContraDesc: TLabel
+    Left = 308
+    Top = 88
+    Width = 217
+    Height = 20
+    AutoSize = False
+    Caption = 'lblContraDesc'
+    Visible = False
+  end
+  object btnOk: TButton
+    Left = 370
+    Top = 143
+    Width = 75
+    Height = 25
+    Caption = '&OK'
+    Default = True
+    ModalResult = 1
+    TabOrder = 1
+  end
+  object btnCancel: TButton
+    Left = 450
+    Top = 143
+    Width = 75
+    Height = 25
+    Caption = 'Cancel'
+    ModalResult = 2
+    TabOrder = 2
+  end
+  object edtBankAccountCode: TEdit
+    Left = 144
+    Top = 85
+    Width = 130
+    Height = 21
+    TabOrder = 0
+    OnChange = edtBankAccountCodeChange
+    OnKeyPress = edtBankAccountCodeKeyPress
+  end
+end
---
+++ Practice/Win32/ContraCodeEntryfrm.pas
@@ -0,0 +1,181 @@
+unit ContraCodeEntryfrm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, OSFont, StdCtrls, Buttons;
+
+type
+  TfrmContraCodeEntry = class(TForm)
+    Label1: TLabel;
+    btnOk: TButton;
+    btnCancel: TButton;
+    Label2: TLabel;
+    edtBankAccountCode: TEdit;
+    sbtnChart: TSpeedButton;
+    lblContraDesc: TLabel;
+    procedure FormCreate(Sender: TObject);
+    procedure sbtnChartClick(Sender: TObject);
+    procedure edtBankAccountCodeKeyPress(Sender: TObject; var Key: Char);
+    procedure edtBankAccountCodeChange(Sender: TObject);
+  private
+    procedure LookupContraCode;
+    function GetContraCode: String;
+  public
+    class function EnterContraCode(out ContraCode: string): Boolean; overload; static;
+    class function EnterContraCode(PopupParent: TCustomForm; out ContraCode: string): Boolean; overload; static;
+
+    property ContraCode: String read GetContraCode;
+  end;
+
+var
+  frmContraCodeEntry: TfrmContraCodeEntry;
+
+implementation
+
+uses
+  AccountLookupFrm, imagesfrm, STDHINTS, bkConst, Globals, BKDEFS;
+  
+{$R *.dfm}
+
+class function TfrmContraCodeEntry.EnterContraCode(PopupParent: TCustomForm; out ContraCode: string): Boolean;
+var
+  EntryForm: TfrmContraCodeEntry;
+begin
+  EntryForm := TfrmContraCodeEntry.Create(nil);
+
+  try
+    EntryForm.PopupParent := PopupParent;
+    EntryForm.PopupMode := pmExplicit;
+    
+    if EntryForm.ShowModal = mrOk then
+    begin
+      ContraCode := EntryForm.ContraCode;
+
+      Result := True;
+    end
+    else
+    begin
+      Result := False;
+    end;
+  finally
+    EntryForm.Free;
+  end;
+end;
+
+procedure TfrmContraCodeEntry.FormCreate(Sender: TObject);
+begin
+  ActiveControl := edtBankAccountCode;
+  
+  ImagesFrm.AppImages.Coding.GetBitmap(CODING_CHART_BMP, sbtnChart.Glyph);
+
+  edtBankAccountCode.MaxLength := MaxBk5CodeLen;
+  edtBankAccountCode.Hint :=
+                    'Enter the Contra Account Code to use for this Bank Account|' +
+                    'Enter the Contra Account Code from the Chart of Accounts which corresponds to this Bank Account';
+
+  sbtnChart.Hint := ChartLookupHint;
+end;
+
+function TfrmContraCodeEntry.GetContraCode: String;
+begin
+  Result := edtBankAccountCode.Text;
+end;
+
+procedure TfrmContraCodeEntry.LookupContraCode;
+var
+  ContraCode: String;
+begin
+  ContraCode := edtBankAccountCode.Text;
+  
+  if PickAccount(ContraCode) then
+  begin
+    edtBankAccountCode.Text := ContraCode;
+
+    edtBankAccountCode.SelectAll;
+  end;
+end;
+
+procedure TfrmContraCodeEntry.sbtnChartClick(Sender: TObject);
+begin
+  LookupContraCode;
+end;
+
+procedure TfrmContraCodeEntry.edtBankAccountCodeChange(Sender: TObject);
+var
+  pAcct: pAccount_Rec;
+  ContraCode: string;
+  IsValid: boolean;
+begin
+  if MyClient.clChart.ItemCount = 0 then
+  begin
+    Exit;
+  end;
+
+  ContraCode := Trim(edtBankAccountCode.Text);
+
+  pAcct:= MyClient.clChart.FindCode(ContraCode);
+
+  IsValid := Assigned( pAcct) and (pAcct^.chAccount_Type = atBankAccount) and pAcct^.chPosting_Allowed;
+
+  if Assigned( pAcct) then
+  begin
+    lblContraDesc.Caption := pAcct^.chAccount_Description;
+    lblContraDesc.Visible := True;
+  end
+  else
+  begin
+    lblContraDesc.Visible := False;
+  end;
+
+  if (ContraCode = '') or (IsValid) then
+  begin
+    edtBankAccountCode.Font.Color := clWindowText;
+    edtBankAccountCode.Color := clWindow;
+    ContraCode := edtBankAccountCode.Text;
+
+    edtBankAccountCode.BorderStyle := bsNone;
+    edtBankAccountCode.BorderStyle := bsSingle;
+
+    edtBankAccountCode.Text := ContraCode;
+  end
+  else
+  begin
+    edtBankAccountCode.Font.Color := clWhite;
+    edtBankAccountCode.Color      := clRed;
+  end;
+end;
+
+procedure TfrmContraCodeEntry.edtBankAccountCodeKeyPress(Sender: TObject; var Key: Char);
+begin
+  if ((Key='-') and (myClient.clFields.clUse_Minus_As_Lookup_Key)) then
+  begin
+    Key := #0;
+
+    LookupContraCode;
+  end;
+end;
+
+class function TfrmContraCodeEntry.EnterContraCode(out ContraCode: string): Boolean;
+var
+  EntryForm: TfrmContraCodeEntry;
+begin
+  EntryForm := TfrmContraCodeEntry.Create(nil);
+
+  try
+    if EntryForm.ShowModal = mrOk then
+    begin
+      ContraCode := EntryForm.ContraCode;
+
+      Result := True;
+    end
+    else
+    begin
+      Result := False;
+    end;
+  finally
+    EntryForm.Free;
+  end;
+end;
+end.
