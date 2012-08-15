8474259fa85be9b13c62f493194b0bb4dd65c55c
BANKLINK\Kerry.Convery <BANKLINK\Kerry.Convery>
2012-08-15 15:44:55 +1200

    Added new methods EncryptStr128/DecryptStr128

--- Practice/Win32/cryptutils.pas
+++ Practice/Win32/cryptutils.pas
@@ -19,10 +19,13 @@ interface
 
 type
    Str16        = string[16];
+   Str128       = string[128];
 
   function EncryptPass16(BFKey: string; PlainText: Str16) : string;
   function DecryptPass16(BFKey: string; Encrypted: string) : Str16;
-
+  function EncryptStr128(BFKey: string; PlainText: Str128) : Str128;
+  function DecryptStr128(BFKey: string; Encrypted: Str128) : Str128;
+  
 //******************************************************************************
 implementation
 uses
@@ -152,6 +155,93 @@ begin
    finally
       BlowFish2.Free;
    end;
+end;
+
+function EncryptStr128(BFKey: string; PlainText: Str128) : Str128;
+var
+   InArray   : TCryptoArray;
+   OutArray  : TCryptoArray;
+   BlowFish2 : TBlowFish;
+
+   PaddedPassword : string;
+   i         : integer;
+   len       : integer;
+begin
+   Result := '';
+   if PlainText = '' then exit;
+   //pad password
+   PaddedPassword := PlainText;
+   While Length( PaddedPassword) < 128 do
+      PaddedPassword := PaddedPassword + #170;
+   //encrypt
+   FillChar(InArray, Sizeof(InArray), #0);
+   FillChar(OutArray, Sizeof(OutArray), #0);
+   for i := 1 to Length( PaddedPassword) do
+      InArray[ i - 1] := PaddedPassword[ i];
+   BlowFish2 := TBlowFish.Create(Nil);
+   try
+      with BlowFish2 do begin
+         CipherMode    := ECBMode;
+         Key           := BFKey;
+         InputType     := SourceByteArray;
+         InputLength   := Length(PaddedPassword);
+         pInputArray   := @InArray;
+         pOutputArray  := @OutArray;
+         EncipherData(false);
+         //encrypted data will now be in OutArray
+         //convert to hex string
+         result := '';
+         Len := length( PaddedPassword);
+         for i := 0 to Pred( Len) do begin
+            result := result + B2H( Byte( OutArray[ i]));
+         end;
+      end;
+   finally
+      BlowFish2.Free;
+   end;
+end;
+
+function DecryptStr128(BFKey: string; Encrypted: Str128) : Str128;
+var
+   InArray   : TCryptoArray;
+   OutArray  : TCryptoArray;
+   BlowFish2 : TBlowFish;
+   Len,i     : integer;
+   S         : string;
+begin
+   Result := '';
+   if Encrypted = '' then exit;
+   //determine length of hex string, must be even number of bytes
+   Len := Length( Encrypted);
+   if Len mod 2 <> 0 then
+      Exit;
+   Len := Len div 2;
+   //convert hex string to byte array
+   FillChar(InArray, Sizeof(InArray), #0);
+   FillChar(OutArray, Sizeof(OutArray), #0);
+   for i := 0 to Pred( Len) do
+      InArray[ i] := Chr( H2B( Copy( Encrypted, (i * 2) +1 , 2)));
+   //decrypt
+   BlowFish2 := TBlowFish.Create(Nil);
+   try
+      with BlowFish2 do begin
+         CipherMode    := ECBMode;
+         Key           := BFKey;
+         InputType     := SourceByteArray;
+         InputLength   := Len;
+         pInputArray   := @InArray;
+         pOutputArray  := @OutArray;
+         DecipherData(false);
+      end;
+      S := OutArray;
+   finally
+      BlowFish2.Free;
+   end;
+   //extract password from string
+   i := Pos( PadChar, S);
+   if i > 0 then
+      S := Copy( S, 0, i - 1);
+   result := S;
 end;
 //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 End.
