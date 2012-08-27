f8431a4a65b69979492ea70aaebc4d53607f6c68
BANKLINK\Tony.Kelly <BANKLINK\Tony.Kelly>
2012-08-27 18:17:36 +1200

    Task 56870 (PR01b of UK Customer Authority Form/Scenario 55696): Select Institution window

--- Practice/Authority Forms/SelectInstitutionfrm.dfm
+++ Practice/Authority Forms/SelectInstitutionfrm.dfm
@@ -1,9 +1,9 @@
 object frmSelectInstitution: TfrmSelectInstitution
   Left = 0
   Top = 0
-  Caption = 'frmSelectInstitution'
-  ClientHeight = 290
-  ClientWidth = 554
+  Caption = 'Select Institution'
+  ClientHeight = 146
+  ClientWidth = 327
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -11,6 +11,48 @@ object frmSelectInstitution: TfrmSelectInstitution
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
+  Position = poMainFormCenter
+  OnKeyDown = FormKeyDown
   PixelsPerInch = 96
   TextHeight = 13
+  object Label1: TLabel
+    Left = 16
+    Top = 16
+    Width = 282
+    Height = 26
+    Caption = 
+      'Select the financial institution that the Customer Authority For' +
+      'm will be generated for:'
+    WordWrap = True
+  end
+  object cmbInstitution: TComboBox
+    Left = 16
+    Top = 64
+    Width = 291
+    Height = 21
+    ItemHeight = 13
+    ItemIndex = 0
+    TabOrder = 0
+    Text = 'Other'
+    Items.Strings = (
+      'Other'
+      'HSBC')
+  end
+  object btnOK: TButton
+    Left = 151
+    Top = 104
+    Width = 75
+    Height = 25
+    Caption = 'OK'
+    TabOrder = 1
+    OnClick = btnOKClick
+  end
+  object btnCancel: TButton
+    Left = 232
+    Top = 104
+    Width = 75
+    Height = 25
+    Caption = 'Cancel'
+    TabOrder = 2
+  end
 end
--- Practice/Authority Forms/SelectInstitutionfrm.pas
+++ Practice/Authority Forms/SelectInstitutionfrm.pas
@@ -4,10 +4,16 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs;
+  Dialogs, StdCtrls;
 
 type
   TfrmSelectInstitution = class(TForm)
+    Label1: TLabel;
+    cmbInstitution: TComboBox;
+    btnOK: TButton;
+    btnCancel: TButton;
+    procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure btnOKClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -19,6 +25,77 @@ var
 
 implementation
 
+uses NewCAFfrm, Globals;
+
 {$R *.dfm}
+
+procedure TfrmSelectInstitution.btnOKClick(Sender: TObject);
+var
+  CAFForm: TfrmNewCAF;
+begin
+  CAFForm := TfrmNewCAF.Create(Application.MainForm);
+
+  try
+    if Screen.DesktopHeight < 635 then
+      CAFForm.Height := Screen.DesktopHeight
+    else
+      CAFForm.Height := 635;
+
+    if Screen.DesktopWidth < 560 then
+      CAFForm.Width := Screen.DesktopWidth
+    else
+      CAFForm.Width := 560;
+
+    if (cmbInstitution.ItemIndex > 0) then // Anything except 'Other', in which case we expect the user to enter the bank themselves
+    begin
+      CAFForm.edtBank.Text := cmbInstitution.Items[cmbInstitution.ItemIndex];
+      CAFForm.edtBank.Enabled := False;
+    end;
+
+    if (CAFForm.edtBank.Text = 'HSBC') then
+    begin
+      // Show extra HSBC stuff
+      CAFForm.edtAccountSignatory1.Visible := True;
+      CAFForm.edtAccountSignatory2.Visible := True;
+      CAFForm.lblAccountSignatories.Visible := True;
+      CAFForm.edtAddressLine1.Visible := True;
+      CAFForm.edtAddressLine2.Visible := True;
+      CAFForm.edtAddressLine3.Visible := True;
+      CAFForm.edtAddressLine4.Visible := True;
+      CAFForm.lblAddressLine1.Visible := True;
+      CAFForm.lblAddressLine2.Visible := True;
+      CAFForm.lblAddressLine3.Visible := True;
+      CAFForm.lblAddressLine4.Visible := True;
+      CAFForm.edtPostalCode.Visible := True;
+      CAFForm.lblPostalCode.Visible := True;
+    end else
+    begin
+      CAFForm.chkSupplyAccount.Top := CAFForm.edtBank.Top + 50;
+      CAFForm.lblServiceFrequency.Top := CAFForm.chkSupplyAccount.Top + 29;
+      CAFForm.pnlServiceFrequency.Top := CAFForm.chkSupplyAccount.Top + 28;
+      CAFForm.pnlBottom.Top := CAFForm.pnlServiceFrequency.Top + 22;
+      CAFForm.Height := CAFForm.Height - 275;
+    end;
+
+    CAFForm.edtPracticeName.Text := AdminSystem.fdFields.fdPractice_Name_for_Reports;
+    CAFForm.edtPracticeCode.Text := AdminSystem.fdFields.fdBankLink_Code;
+
+    CAFForm.ShowModal;
+  finally
+    Free;
+  end;
+
+end;
+
+procedure TfrmSelectInstitution.FormKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+  case Key of
+    VK_RETURN:
+      btnOK.Click;
+    VK_ESCAPE:
+      btnCancel.Click;
+  end;
+end;
 
 end.
