ce9d3956cb5220154ef290ba1b22d00ae8b2d8df
BANKLINK\Tony.Kelly <BANKLINK\Tony.Kelly>
2012-08-27 14:56:22 +1200

    Select Institution Window for UK Customer Authority Form

---
+++ Practice/Authority Forms/SelectInstitutionfrm.pas
@@ -0,0 +1,24 @@
+unit SelectInstitutionfrm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TfrmSelectInstitution = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmSelectInstitution: TfrmSelectInstitution;
+
+implementation
+
+{$R *.dfm}
+
+end.
