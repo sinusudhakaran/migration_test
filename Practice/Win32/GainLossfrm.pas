d18d6dcec71c3bc036e39cf765f74564dc522fdd
BANKLINK\Tony.Kelly <BANKLINK\Tony.Kelly>
2012-11-14 10:03:28 +1300

    Adding Exchange Gain/Losses Entries Screen (PR03b of Difference on Exchange Wizard spec, Scenario 61959)

---
+++ Practice/Win32/GainLossfrm.pas
@@ -0,0 +1,24 @@
+unit GainLossfrm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs;
+
+type
+  TfrmGainLoss = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmGainLoss: TfrmGainLoss;
+
+implementation
+
+{$R *.dfm}
+
+end.
