de8aac1b0db986c74111705a1ec769559fd73577
BANKLINK\Ralph.Austen <BANKLINK\Ralph.Austen>
2013-03-20 13:32:28 +1300

    Reajusted amount columns so they show numbers correctly

--- Practice/Printing/RptContractorPayee.pas
+++ Practice/Printing/RptContractorPayee.pas
@@ -542,10 +542,10 @@ begin
   AddColAuto(Job, cLeft, 8, Gcgap,'Date', jtLeft);
   AddColAuto(Job, cLeft, 8, Gcgap,'Reference', jtLeft);
   AddColAuto(Job, cLeft, 10,Gcgap,'Account', jtLeft);
-  AddColAuto(Job, cLeft, 44, Gcgap,'Narration', jtLeft);
-  AddFormatColAuto(Job,cLeft,12, Gcgap,'No ABN Withholding Tax',jtRight,'#,##0.00;(#,##0.00);-', MyClient.FmtMoneyStrBrackets, true);
-  AddFormatColAuto(Job,cLeft,10, Gcgap,'Total GST',jtRight,'#,##0.00;(#,##0.00);-', MyClient.FmtMoneyStrBrackets, true);
-  AddFormatColAuto(Job,cLeft,16, Gcgap,'Gross Amount Paid (including GST and any tax withheld)',jtRight,'#,##0.00;(#,##0.00);-', MyClient.FmtMoneyStrBrackets, true);
+  AddColAuto(Job, cLeft, 34, Gcgap,'Narration', jtLeft);
+  AddFormatColAuto(Job,cLeft,14, Gcgap,'No ABN Withholding Tax',jtRight,'#,##0.00;(#,##0.00);-', MyClient.FmtMoneyStrBrackets, true);
+  AddFormatColAuto(Job,cLeft,12, Gcgap,'Total GST',jtRight,'#,##0.00;(#,##0.00);-', MyClient.FmtMoneyStrBrackets, true);
+  AddFormatColAuto(Job,cLeft,14, Gcgap,'Gross Amount Paid (including GST and any tax withheld)',jtRight,'#,##0.00;(#,##0.00);-', MyClient.FmtMoneyStrBrackets, true);
 
   //Add Footers
   AddCommonFooter(Job);
