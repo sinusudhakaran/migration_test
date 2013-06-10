d4c595176f62105ac5452f74b7d8ed200f25f320
BANKLINK\Kerry.Convery <BANKLINK\Kerry.Convery>
2013-06-11 11:06:44 +1200

    Changed TwnFdCSV to TwnfdCSV

--- Common/Lib/ExtractCommon.pas
+++ Common/Lib/ExtractCommon.pas
@@ -229,7 +229,7 @@ const                   // 12345678
   BBTechBasicCode       = 'BBTech';
   ClassSuperBasicCode   = 'ClssSupr';
   IDSSuper              = 'IDSSuper';
-  TwinField             = 'TwnFdCSV';
+  TwinField             = 'TwnfdCSV';
 
 implementation
 
