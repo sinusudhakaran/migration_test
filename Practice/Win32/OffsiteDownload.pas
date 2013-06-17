8f2ce985112767bff6c08502f70f4a465d9369e7
BANKLINK\Kerry.Convery <BANKLINK\Kerry.Convery>
2013-06-17 13:45:55 +1200

    Fixed spelling mistake

--- Practice/Win32/OffsiteDownload.pas
+++ Practice/Win32/OffsiteDownload.pas
@@ -691,7 +691,7 @@ begin
     except
       on E : EDownloadVerify do
       begin
-         HelpfulErrorMsg('An error occured while verifying the downloaded data.'+#13+#13+
+         HelpfulErrorMsg('An error occurred while verifying the downloaded data.'+#13+#13+
                          E.Message+ #13+#13+
                          'Please contact '+SHORTAPPNAME+' support.',0);
       end;
