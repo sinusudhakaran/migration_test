0c0b952a87e7e58fbc6ffd1fcf96d7209554f226
BANKLINK\Tony.Kelly <BANKLINK\Tony.Kelly>
2014-01-17 09:06:48 +1300

    Adding Bulk Export to Practice Beta Build search path for BGL 360 unit test

--- Practice/Win32/bk5testconsole.dpr
+++ Practice/Win32/bk5testconsole.dpr
@@ -15,8 +15,7 @@ uses
   utAuditTrail in '..\Tests\utAuditTrail.pas',
   utBudgetImportExport in '..\Tests\utBudgetImportExport.pas',
   utBGL360 in '..\Tests\utBGL360.pas',
-  utSimpleFundX in '..\Tests\utSimpleFundX.pas' {/utSimpleFund360Bulk in '..\Tests\utSimpleFund360Bulk.pas';},
-  utInstitutionCol in '..\Tests\utInstitutionCol.pas',
+  utSimpleFundX in '..\Tests\utSimpleFundX.pas',  utSimpleFund360Bulk in '..\Tests\utSimpleFund360Bulk.pas',  utInstitutionCol in '..\Tests\utInstitutionCol.pas',
   utToDo in '..\Tests\utToDo.pas',
   utBudget in '..\Tests\utBudget.pas';
 
