17ced1d6c96fb175118bce7bafe4c180f4ea425d
BANKLINK\Tony.Kelly <BANKLINK\Tony.Kelly>
2014-03-20 11:05:17 +1300

    US8009: Fixing unit test

--- Practice/Tests/utRecommendedMems.pas
+++ Practice/Tests/utRecommendedMems.pas
@@ -239,6 +239,74 @@ begin
   pT.txType := 60;
   pT.txCoded_By := cbManual;
   TestAccount.baTransaction_List.Insert_Transaction_Rec( pT);
+
+  pT := TestAccount.baTransaction_List.New_Transaction;
+  pT^.txDate_Presented  := Apr05_2004;
+  pT.txDate_Effective   := Apr05_2004;
+  pT.txAmount           := 6000;
+  pT.txAccount          := '232';
+  pT.txNotes            := 'NOTE';
+  pT.txGL_Narration     := 'Account 1 Tran11';
+  pT.txStatement_Details := 'Details 3';
+  pT.txTax_Invoice_Available   := false;
+  pT.txPayee_Number     := 0;
+  pT.txGST_Class := 1;
+  pt.txGST_Amount := Round(12000 / 9);
+  pT.txExternal_GUID := '20';
+  pT.txType := 70;
+  pT.txCoded_By := cbManual;
+  TestAccount.baTransaction_List.Insert_Transaction_Rec( pT);
+
+  pT := TestAccount.baTransaction_List.New_Transaction;
+  pT^.txDate_Presented  := Apr05_2004;
+  pT.txDate_Effective   := Apr05_2004;
+  pT.txAmount           := 6000;
+  pT.txAccount          := '232';
+  pT.txNotes            := 'NOTE';
+  pT.txGL_Narration     := 'Account 1 Tran12';
+  pT.txStatement_Details := 'Details 3';
+  pT.txTax_Invoice_Available   := false;
+  pT.txPayee_Number     := 0;
+  pT.txGST_Class := 1;
+  pt.txGST_Amount := Round(12000 / 9);
+  pT.txExternal_GUID := '20';
+  pT.txType := 70;
+  pT.txCoded_By := cbManual;
+  TestAccount.baTransaction_List.Insert_Transaction_Rec( pT);
+
+  pT := TestAccount.baTransaction_List.New_Transaction;
+  pT^.txDate_Presented  := Apr05_2004;
+  pT.txDate_Effective   := Apr05_2004;
+  pT.txAmount           := 6000;
+  pT.txAccount          := '232';
+  pT.txNotes            := 'NOTE';
+  pT.txGL_Narration     := 'Account 1 Tran13';
+  pT.txStatement_Details := 'Details 3';
+  pT.txTax_Invoice_Available   := false;
+  pT.txPayee_Number     := 0;
+  pT.txGST_Class := 1;
+  pt.txGST_Amount := Round(12000 / 9);
+  pT.txExternal_GUID := '20';
+  pT.txType := 70;
+  pT.txCoded_By := cbManual;
+  TestAccount.baTransaction_List.Insert_Transaction_Rec( pT);
+
+  pT := TestAccount.baTransaction_List.New_Transaction;
+  pT^.txDate_Presented  := Apr05_2004;
+  pT.txDate_Effective   := Apr05_2004;
+  pT.txAmount           := 6000;
+  pT.txAccount          := '';
+  pT.txNotes            := 'NOTE';
+  pT.txGL_Narration     := 'Account 1 Tran14';
+  pT.txStatement_Details := 'Details 3';
+  pT.txTax_Invoice_Available   := false;
+  pT.txPayee_Number     := 0;
+  pT.txGST_Class := 1;
+  pt.txGST_Amount := Round(12000 / 9);
+  pT.txExternal_GUID := '20';
+  pT.txType := 70;
+  pT.txCoded_By := cbNotCoded;
+  TestAccount.baTransaction_List.Insert_Transaction_Rec( pT);
 end;
 
 procedure TUnitTestRecommendedMems.SetUp;
@@ -314,7 +382,27 @@ begin
     Check(utBankAccount = '12345');
     Check(utSequenceNo = 10);
 
-    Check(RecommendedMems.Unscanned.ItemCount = 10);
+    utBankAccount := RecommendedMems.Unscanned.Unscanned_Transaction_At(10).utFields.utBank_Account_Number;
+    utSequenceNo  := RecommendedMems.Unscanned.Unscanned_Transaction_At(10).utFields.utSequence_No;
+    Check(utBankAccount = '12345');
+    Check(utSequenceNo = 11);
+
+    utBankAccount := RecommendedMems.Unscanned.Unscanned_Transaction_At(11).utFields.utBank_Account_Number;
+    utSequenceNo  := RecommendedMems.Unscanned.Unscanned_Transaction_At(11).utFields.utSequence_No;
+    Check(utBankAccount = '12345');
+    Check(utSequenceNo = 12);
+
+    utBankAccount := RecommendedMems.Unscanned.Unscanned_Transaction_At(12).utFields.utBank_Account_Number;
+    utSequenceNo  := RecommendedMems.Unscanned.Unscanned_Transaction_At(12).utFields.utSequence_No;
+    Check(utBankAccount = '12345');
+    Check(utSequenceNo = 13);
+
+    utBankAccount := RecommendedMems.Unscanned.Unscanned_Transaction_At(13).utFields.utBank_Account_Number;
+    utSequenceNo  := RecommendedMems.Unscanned.Unscanned_Transaction_At(13).utFields.utSequence_No;
+    Check(utBankAccount = '12345');
+    Check(utSequenceNo = 14);
+
+    Check(RecommendedMems.Unscanned.ItemCount = 14);
 
     RecommendedMems.MemScan(True, TestAccount);
 
@@ -322,11 +410,11 @@ begin
     Check(RecommendedMems.Unscanned.ItemCount = 0);
 
     // Candidate Processing
-    Check(RecommendedMems.Candidate.cpFields.cpCandidate_ID_To_Process = 6);
-    Check(RecommendedMems.Candidate.cpFields.cpNext_Candidate_ID = 6);
+    Check(RecommendedMems.Candidate.cpFields.cpCandidate_ID_To_Process = 8);
+    Check(RecommendedMems.Candidate.cpFields.cpNext_Candidate_ID = 8);
 
     // Candidates
-    Check(RecommendedMems.Candidates.ItemCount = 5);
+    Check(RecommendedMems.Candidates.ItemCount = 7);
 
     // Candidate 0
     Check(RecommendedMems.Candidates.Candidate_Mem_At(0).cmFields.cmRecord_Type = 167);
@@ -373,20 +461,38 @@ begin
     Check(RecommendedMems.Candidates.Candidate_Mem_At(4).cmFields.cmAccount = '230');
     Check(RecommendedMems.Candidates.Candidate_Mem_At(4).cmFields.cmStatement_Details = 'Details 2');
 
+    // Candidate 5
+    Check(RecommendedMems.Candidates.Candidate_Mem_At(5).cmFields.cmRecord_Type = 167);
+    Check(RecommendedMems.Candidates.Candidate_Mem_At(5).cmFields.cmCount = 1);
+    Check(RecommendedMems.Candidates.Candidate_Mem_At(5).cmFields.cmType = 70);
+    Check(RecommendedMems.Candidates.Candidate_Mem_At(5).cmFields.cmBank_Account_Number = '12345');
+    Check(RecommendedMems.Candidates.Candidate_Mem_At(5).cmFields.cmCoded_By = cbNotCoded);
+    Check(RecommendedMems.Candidates.Candidate_Mem_At(5).cmFields.cmAccount = '');
+    Check(RecommendedMems.Candidates.Candidate_Mem_At(5).cmFields.cmStatement_Details = 'Details 3');
+
+    // Candidate 6
+    Check(RecommendedMems.Candidates.Candidate_Mem_At(6).cmFields.cmRecord_Type = 167);
+    Check(RecommendedMems.Candidates.Candidate_Mem_At(6).cmFields.cmCount = 3);
+    Check(RecommendedMems.Candidates.Candidate_Mem_At(6).cmFields.cmType = 70);
+    Check(RecommendedMems.Candidates.Candidate_Mem_At(6).cmFields.cmBank_Account_Number = '12345');
+    Check(RecommendedMems.Candidates.Candidate_Mem_At(6).cmFields.cmCoded_By = cbManual);
+    Check(RecommendedMems.Candidates.Candidate_Mem_At(6).cmFields.cmAccount = '232');
+    Check(RecommendedMems.Candidates.Candidate_Mem_At(6).cmFields.cmStatement_Details = 'Details 3');
+
     // Recommended Mems (only one for this set of data)
     Check(RecommendedMems.Recommended.ItemCount = 1);
 
     // Recommendation 0
     Check(RecommendedMems.Recommended.Recommended_Mem_At(0).rmFields.rmRecord_Type = 168);
-    Check(RecommendedMems.Recommended.Recommended_Mem_At(0).rmFields.rmType = 30);
+    Check(RecommendedMems.Recommended.Recommended_Mem_At(0).rmFields.rmType = 70);
     Check(RecommendedMems.Recommended.Recommended_Mem_At(0).rmFields.rmBank_Account_Number = '12345');
-    Check(RecommendedMems.Recommended.Recommended_Mem_At(0).rmFields.rmAccount = '230');
-    Check(RecommendedMems.Recommended.Recommended_Mem_At(0).rmFields.rmStatement_Details = 'Details 1');
-    Check(RecommendedMems.Recommended.Recommended_Mem_At(0).rmFields.rmManual_Count = 4);
+    Check(RecommendedMems.Recommended.Recommended_Mem_At(0).rmFields.rmAccount = '232');
+    Check(RecommendedMems.Recommended.Recommended_Mem_At(0).rmFields.rmStatement_Details = 'Details 3');
+    Check(RecommendedMems.Recommended.Recommended_Mem_At(0).rmFields.rmManual_Count = 3);
     Check(RecommendedMems.Recommended.Recommended_Mem_At(0).rmFields.rmUncoded_Count = 1);
 
     // Removing a recommendation
-    RecommendedMems.RemoveRecommendedMems('12345', 30, 'Details 1');
+    RecommendedMems.RemoveRecommendedMems('12345', 70, 'Details 3');
     Check(RecommendedMems.Recommended.ItemCount = 0);
 
     // Removing an account from recommendations
