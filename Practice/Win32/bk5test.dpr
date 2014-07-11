920a7806b6b684fc9721935cfcb096834f5b6f76
MYOB\Ralph.Austen <MYOB\Ralph.Austen>
2014-07-11 14:42:33 +1200

    removed commented code

--- Practice/Win32/bk5test.dpr
+++ Practice/Win32/bk5test.dpr
@@ -5,7 +5,6 @@ uses
   LockUtils,
   TestFrameWork,
   GUITestRunner,
-  //ImagesFrm,
   utFileExtensionUtils in '..\Tests\utFileExtensionUtils.pas',
   utGUIReportSchedule in '..\Tests\utGUIReportSchedule.pas',
   utMemorisationsObj in '..\Tests\utMemorisationsObj.pas',
@@ -31,7 +30,6 @@ uses
 begin
   InitLocking;
   Application.Initialize;
-  //Application.CreateForm(TAppImages, AppImages);
   GUITestRunner.RunRegisteredTests;
 end.
 
