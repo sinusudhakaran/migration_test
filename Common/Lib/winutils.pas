7902229550f00d6ba457e54ea8f592d7c406b88a
MYOB\Jarno.Noordermeer <MYOB\Jarno.Noordermeer>
2015-02-02 13:55:07 +1300

    Copyright 2015 changes

--- Common/Lib/winutils.pas
+++ Common/Lib/winutils.pas
@@ -292,7 +292,7 @@ begin
 {$IFDEF bkUseFinalBuilderVer}
   Result := VERYEAR;
 {$ELSE}
-  Result := '2014';
+  Result := '2015';
 {$ENDIF}
 end;
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
