d2fa7b0f621ca26f90896f4f5c042753b9e118f0
MYOB\Sinu.Sudhakaran <MYOB\Sinu.Sudhakaran>
2015-10-27 10:03:33 +1300

    my.MYOB sign in screen added for practice ledger extract

---
+++ Practice/Win32/myMYOBSignInFrm.dfm
@@ -0,0 +1,262 @@
+object myMYOBSignInForm: TmyMYOBSignInForm
+  Left = 0
+  Top = 0
+  BorderIcons = [biSystemMenu]
+  BorderStyle = bsSingle
+  Caption = ' '
+  ClientHeight = 324
+  ClientWidth = 567
+  Color = clWindow
+  ParentFont = True
+  OldCreateOrder = False
+  Position = poScreenCenter
+  OnCreate = FormCreate
+  OnDestroy = FormDestroy
+  OnShow = FormShow
+  PixelsPerInch = 96
+  TextHeight = 13
+  object pnlLogin: TPanel
+    Left = 0
+    Top = 0
+    Width = 567
+    Height = 154
+    Align = alTop
+    BevelOuter = bvNone
+    ParentColor = True
+    TabOrder = 0
+    object lblEmail: TLabel
+      Left = 24
+      Top = 27
+      Width = 31
+      Height = 16
+      Caption = 'Email'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -13
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentFont = False
+    end
+    object lblPassword: TLabel
+      Left = 24
+      Top = 69
+      Width = 55
+      Height = 16
+      Caption = 'Password'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -13
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentFont = False
+    end
+    object lblForgotPassword: TLabel
+      Left = 128
+      Top = 120
+      Width = 131
+      Height = 16
+      Caption = 'Forgot your password?'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clBlue
+      Font.Height = -13
+      Font.Name = 'Tahoma'
+      Font.Style = [fsUnderline]
+      ParentFont = False
+      OnClick = lblForgotPasswordClick
+    end
+    object edtEmail: TEdit
+      Left = 128
+      Top = 24
+      Width = 425
+      Height = 24
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -13
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentFont = False
+      TabOrder = 0
+    end
+    object edtPassword: TEdit
+      Left = 128
+      Top = 66
+      Width = 169
+      Height = 24
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -13
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentFont = False
+      PasswordChar = '#'
+      TabOrder = 1
+    end
+    object btnSignIn: TButton
+      Left = 428
+      Top = 117
+      Width = 125
+      Height = 25
+      Caption = 'my.MYOB Sign in'
+      Default = True
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -13
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentFont = False
+      TabOrder = 2
+      OnClick = btnSignInClick
+    end
+  end
+  object pnlClientSelection: TPanel
+    Left = 0
+    Top = 210
+    Width = 567
+    Height = 66
+    Align = alClient
+    BevelOuter = bvNone
+    ParentColor = True
+    TabOrder = 1
+    Visible = False
+    object ShapeBorder: TShape
+      Left = 0
+      Top = 0
+      Width = 567
+      Height = 1
+      Align = alTop
+      Pen.Color = clSilver
+      ExplicitLeft = 24
+      ExplicitTop = 24
+      ExplicitWidth = 65
+    end
+    object Label1: TLabel
+      Left = 24
+      Top = 21
+      Width = 82
+      Height = 16
+      AutoSize = False
+      Caption = 'Select client'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -13
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentFont = False
+    end
+    object cmbSelectClient: TComboBox
+      Left = 128
+      Top = 18
+      Width = 425
+      Height = 24
+      Style = csDropDownList
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -13
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ItemHeight = 16
+      ParentFont = False
+      TabOrder = 0
+    end
+  end
+  object pnlControls: TPanel
+    Left = 0
+    Top = 276
+    Width = 567
+    Height = 48
+    Align = alBottom
+    BevelOuter = bvNone
+    Caption = ' '
+    ParentBackground = False
+    TabOrder = 2
+    DesignSize = (
+      567
+      48)
+    object Shape2: TShape
+      Left = 0
+      Top = 0
+      Width = 567
+      Height = 1
+      Align = alTop
+      Pen.Color = clSilver
+      ExplicitLeft = 24
+      ExplicitTop = 24
+      ExplicitWidth = 65
+    end
+    object btnOK: TButton
+      Left = 394
+      Top = 13
+      Width = 75
+      Height = 25
+      Anchors = [akRight, akBottom]
+      Caption = '&OK'
+      Default = True
+      TabOrder = 0
+      Visible = False
+      OnClick = btnOKClick
+    end
+    object btnCancel: TButton
+      Left = 478
+      Top = 13
+      Width = 75
+      Height = 25
+      Anchors = [akRight, akBottom]
+      Cancel = True
+      Caption = 'Cancel'
+      TabOrder = 1
+      OnClick = btnCancelClick
+    end
+  end
+  object pnlFirmSelection: TPanel
+    Left = 0
+    Top = 154
+    Width = 567
+    Height = 56
+    Align = alTop
+    BevelOuter = bvNone
+    Caption = ' '
+    ParentColor = True
+    TabOrder = 3
+    Visible = False
+    object Label6: TLabel
+      Left = 24
+      Top = 24
+      Width = 82
+      Height = 16
+      AutoSize = False
+      Caption = 'Select firm'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -13
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ParentFont = False
+    end
+    object Shape1: TShape
+      Left = 0
+      Top = 0
+      Width = 567
+      Height = 1
+      Align = alTop
+      Pen.Color = clSilver
+      ExplicitLeft = 24
+      ExplicitTop = 24
+      ExplicitWidth = 65
+    end
+    object cmbSelectFirm: TComboBox
+      Left = 128
+      Top = 21
+      Width = 425
+      Height = 24
+      Style = csDropDownList
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -13
+      Font.Name = 'Tahoma'
+      Font.Style = []
+      ItemHeight = 16
+      ParentFont = False
+      TabOrder = 0
+    end
+  end
+end
---
+++ Practice/Win32/myMYOBSignInFrm.pas
@@ -0,0 +1,362 @@
+unit myMYOBSignInFrm;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ExtCtrls, PracticeLedgerObj, CashbookMigrationRestData, OSFont;
+
+type
+  TFormShowType = (fsSignIn, fsSelectFirm, fsSelectClient);
+
+  TmyMYOBSignInForm = class(TForm)
+    pnlLogin: TPanel;
+    lblEmail: TLabel;
+    lblPassword: TLabel;
+    lblForgotPassword: TLabel;
+    edtEmail: TEdit;
+    edtPassword: TEdit;
+    btnSignIn: TButton;
+    pnlClientSelection: TPanel;
+    ShapeBorder: TShape;
+    Label1: TLabel;
+    cmbSelectClient: TComboBox;
+    pnlControls: TPanel;
+    btnOK: TButton;
+    btnCancel: TButton;
+    Shape2: TShape;
+    pnlFirmSelection: TPanel;
+    Label6: TLabel;
+    Shape1: TShape;
+    cmbSelectFirm: TComboBox;
+    procedure btnSignInClick(Sender: TObject);
+    procedure lblForgotPasswordClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure btnOKClick(Sender: TObject);
+    procedure btnCancelClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+  private
+    { Private declarations }
+    FFirms : TFirms;
+    FBusinesses : TBusinesses;
+    FFormShowType : TFormShowType;
+    FShowFirmSelection : Boolean;
+
+    FSelectedName: string;
+    FSelectedID: string;
+
+    procedure ShowConnectionError(aError : string);
+    procedure LoadFirms;
+    procedure LoadBusinesses;
+  public
+    { Public declarations }
+    property FormShowType : TFormShowType read FFormShowType write FFormShowType;
+    property ShowFirmSelection : Boolean read FShowFirmSelection write FShowFirmSelection;
+    property SelectedID: string read FSelectedID write FSelectedID;
+    property SelectedName : string read FSelectedName write FSelectedName;
+  end;
+
+var
+  myMYOBSignInForm: TmyMYOBSignInForm;
+
+const
+  UNITNAME = 'myMYOBSignInFrm';
+
+implementation
+
+uses ShellApi, Globals, CashbookMigration, WarningMoreFrm, bkContactInformation,
+  ErrorMoreFrm, SYDEFS, Admin32, LogUtil, AuditMgr, IniSettings;
+
+{$R *.dfm}
+
+procedure TmyMYOBSignInForm.btnCancelClick(Sender: TObject);
+begin
+  ModalResult := mrCancel;
+end;
+
+procedure TmyMYOBSignInForm.btnOKClick(Sender: TObject);
+var
+  Firm : TFirm;
+  Business : TBusinessData;
+begin
+  if (FormShowType = fsSelectFirm) then
+  begin
+    if (Trim(cmbSelectFirm.Items.Text) <> '') then
+    begin
+      Firm := FFirms.GetItem(cmbSelectFirm.ItemIndex);
+      if Assigned(Firm) then
+      begin
+        FSelectedID := Firm.ID;
+        FSelectedName := Firm.Name;
+      end;
+    end;
+  end
+  else if (FormShowType = fsSelectClient) then
+  begin
+    if (Trim(cmbSelectClient.Items.Text) <> '') then
+    begin
+      Business := FBusinesses.GetItem(cmbSelectClient.ItemIndex);
+      if Assigned(Business) then
+      begin
+        FSelectedID := Business.ID;
+        FSelectedName := Business.Name;
+      end;
+    end;
+  end;
+  ModalResult := mrOk;
+end;
+
+procedure TmyMYOBSignInForm.btnSignInClick(Sender: TObject);
+var
+  sError: string;
+  OldCursor: TCursor;
+  InvalidPass: boolean;
+begin
+  OldCursor := Screen.Cursor;
+  Screen.Cursor := crHourglass;
+  try
+    if UserINI_myMYOB_EmailAddress <> Trim(edtEmail.Text) then
+    begin
+      UserINI_myMYOB_EmailAddress := Trim(edtEmail.Text);
+      WriteUsersINI(CurrUser.Code);
+    end;
+    PracticeLedger.RandomKey := UserINI_myMYOB_Random_Key;
+    PracticeLedger.EncryptToken(UserINI_myMYOB_Access_Token);
+
+    // my.MYOB login
+    if not PracticeLedger.Login(edtEmail.Text, edtPassword.Text, sError, InvalidPass) then
+    begin
+      Screen.Cursor := OldCursor;
+
+      if InvalidPass then
+        HelpfulWarningMsg(sError, 0)
+      else
+        ShowConnectionError(sError);
+      edtEmail.SetFocus;
+    end
+    else
+    begin
+      UserINI_myMYOB_Access_Token := PracticeLedger.UnEncryptedToken;
+      UserINI_myMYOB_Random_Key := PracticeLedger.RandomKey;
+      UserINI_myMYOB_Refresh_Token := PracticeLedger.RefreshToken;
+      UserINI_myMYOB_Expires_TokenAt := PracticeLedger.TokenExpiresAt;
+      WriteUsersINI(CurrUser.Code);
+      if (FormShowType = fsSignIn) and (ShowFirmSelection) then
+      begin
+        // Get Firms
+        FormShowType := fsSelectFirm;
+        if not PracticeLedger.GetFirms(FFirms, sError) then
+        begin
+          Screen.Cursor := OldCursor;
+          ShowConnectionError(sError);
+          ModalResult := mrCancel;
+        end;
+        //pnlLogin.Visible := False;
+        pnlFirmSelection.Visible := True;
+        Self.Height := 300;
+        btnOK.Visible := True;
+
+        LoadFirms;
+      end
+      else if (FormShowType = fsSignIn) and (not ShowFirmSelection) then
+      begin // means show client - for a normal user
+        // Get Businesses
+        FormShowType := fsSelectClient;
+        if not PracticeLedger.GetBusinesses(AdminSystem.fdFields.fdmyMYOBFirmID ,FBusinesses, sError) then
+        begin
+          Screen.Cursor := OldCursor;
+          ShowConnectionError(sError);
+          ModalResult := mrCancel;
+        end;
+        //pnlLogin.Visible := False;
+        pnlClientSelection.Visible := True;
+        Self.Height := 300;
+        btnOK.Visible := True;
+
+        LoadBusinesses;
+      end;
+    end;
+  finally
+    Screen.Cursor := OldCursor;
+  end;
+end;
+
+procedure TmyMYOBSignInForm.FormCreate(Sender: TObject);
+begin
+  FFirms := TFirms.Create;
+  FBusinesses := TBusinesses.Create;
+end;
+
+procedure TmyMYOBSignInForm.FormDestroy(Sender: TObject);
+begin
+  if Assigned(FFirms) then
+  begin
+    FFirms.Clear;
+    FreeAndNil(FFirms);
+  end;
+  if Assigned(FBusinesses) then
+  begin
+    FBusinesses.Clear;
+    FreeAndNil(FBusinesses);
+  end;
+end;
+
+procedure TmyMYOBSignInForm.FormShow(Sender: TObject);
+var
+  sError: string;
+  OldCursor: TCursor;
+begin
+  edtPassword.Text := '';
+  edtEmail.Text := UserINI_myMYOB_EmailAddress;
+  pnlClientSelection.Visible := False;
+  pnlFirmSelection.Visible := False;
+  pnlLogin.Visible := False;
+  btnOK.Visible := True;
+
+  case FormShowType of
+    fsSignIn :
+    begin
+      pnlLogin.Visible := True;
+      Self.Height := 250;
+      btnOK.Visible := False;
+    end;
+    fsSelectFirm :
+    begin
+      pnlFirmSelection.Visible := True;
+      Self.Height := 150;
+      OldCursor := Screen.Cursor;
+      Screen.Cursor := crHourglass;
+      try
+        PracticeLedger.UnEncryptedToken := UserINI_myMYOB_Access_Token;
+        PracticeLedger.RandomKey := UserINI_myMYOB_Random_Key;
+        PracticeLedger.RefreshToken := UserINI_myMYOB_Refresh_Token;
+        Application.ProcessMessages;
+        // Get Firms
+        if not PracticeLedger.GetFirms(FFirms, sError) then
+        begin
+          Screen.Cursor := OldCursor;
+          ShowConnectionError(sError);
+          ModalResult := mrCancel;
+        end;
+        LoadFirms;
+      finally
+        Screen.Cursor := OldCursor;
+      end;
+    end;
+    fsSelectClient :
+    begin
+      pnlClientSelection.Visible := True;
+      Self.Height := 150;
+      OldCursor := Screen.Cursor;
+      Screen.Cursor := crHourglass;
+      try
+        PracticeLedger.UnEncryptedToken := UserINI_myMYOB_Access_Token;
+        PracticeLedger.RandomKey := UserINI_myMYOB_Random_Key;
+        PracticeLedger.RefreshToken := UserINI_myMYOB_Refresh_Token;
+        Application.ProcessMessages;
+        // Get Businesses
+        if not PracticeLedger.GetBusinesses(AdminSystem.fdFields.fdmyMYOBFirmID, FBusinesses, sError) then
+        begin
+          Screen.Cursor := OldCursor;
+          ShowConnectionError(sError);
+          ModalResult := mrCancel;
+        end;
+        LoadBusinesses;
+      finally
+        Screen.Cursor := OldCursor;
+      end;
+    end;
+  end;
+end;
+
+procedure TmyMYOBSignInForm.lblForgotPasswordClick(Sender: TObject);
+var
+  link : string;
+  OldCursor: TCursor;
+begin
+  OldCursor := Screen.Cursor;
+  Screen.Cursor := crHourglass;
+
+  try
+    link := PRACINI_DefaultCashbookForgotPasswordURL;
+
+    if length(link) = 0 then
+      exit;
+
+    ShellExecute(0, 'open', PChar(link), nil, nil, SW_NORMAL);
+  finally
+    Screen.Cursor := OldCursor;
+  end;
+end;
+
+procedure TmyMYOBSignInForm.LoadBusinesses;
+var
+  i, Index: Integer;
+  Business : TBusinessData;
+begin
+  cmbSelectClient.Items.Clear;
+  Index := 0;
+
+  if not Assigned(FBusinesses) then
+    Exit;
+
+  if FBusinesses.Count = 0 then
+    ModalResult := mrCancel;
+
+  for i := 0 to FBusinesses.Count - 1 do
+  begin
+    Business := FBusinesses.GetItem(i);
+    if Assigned(Business) then
+    begin
+      if Business.ID = MyClient.clExtra.cemyMYOBClientIDSelected then
+        Index := i;
+      cmbSelectClient.Items.Add(Business.Name);
+    end;
+  end;
+  cmbSelectClient.ItemIndex := Index;
+  cmbSelectClient.SetFocus;
+end;
+
+procedure TmyMYOBSignInForm.LoadFirms;
+var
+  i, Index: Integer;
+  Firm : TFirm;
+begin
+  cmbSelectFirm.Items.Clear;
+  Index := 0;
+  if not Assigned(FFirms) then
+    Exit;
+
+  if FFirms.Count = 0 then
+    ModalResult := mrCancel;
+
+  for i := 0 to FFirms.Count - 1 do
+  begin
+    Firm := FFirms.GetItem(i);
+    if Assigned(Firm) then
+    begin
+      // Check for Practice Ledger 
+      if Pos('PL',Firm.EligibleLicense) > 0 then
+      begin
+        if (Firm.ID = AdminSystem.fdFields.fdmyMYOBFirmID) then
+          Index := i;
+        cmbSelectFirm.Items.Add(Firm.Name);
+      end;
+    end;
+  end;
+  cmbSelectFirm.ItemIndex := Index;
+  cmbSelectFirm.SetFocus;
+end;
+
+procedure TmyMYOBSignInForm.ShowConnectionError(aError: string);
+var
+  SupportNumber : string;
+begin
+  SupportNumber := TContactInformation.SupportPhoneNo[ AdminSystem.fdFields.fdCountry ];
+  HelpfulErrorMsg('Could not connect to my.MYOB service, please try again later. ' +
+                  'If problem persists please contact ' + SHORTAPPNAME + ' support ' + SupportNumber + '.',
+                  0, false, aError, true);
+end;
+
+end.
