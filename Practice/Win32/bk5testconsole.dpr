58cc57ea6b2d0b93194d5e3fc545a217f19d0c55
MYOB\Sinu.Sudhakaran <MYOB\Sinu.Sudhakaran>
2015-12-02 12:59:03 +1300

    Added Practice ledger test cases

--- Practice/Win32/bk5testconsole.dpr
+++ Practice/Win32/bk5testconsole.dpr
@@ -22,8 +22,8 @@ uses
   utToDo in '..\Tests\utToDo.pas',
   utBudget in '..\Tests\utBudget.pas',
   utTPRExtact in '..\Tests\utTPRExtact.pas',
-  utCashbookChartExport in '..\Tests\utCashbookChartExport.pas';
-
+  utCashbookChartExport in '..\Tests\utCashbookChartExport.pas',
+  utPracticeLedger in '..\Tests\utPracticeLedger.pas';
 {$R *.res}
 
 begin
