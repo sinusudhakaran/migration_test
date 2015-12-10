6a413d263b8ef0f2cc2d33721322f2b596368cee
MYOB\Sinu.Sudhakaran <MYOB\Sinu.Sudhakaran>
2015-12-11 08:46:14 +1300

    Fixed the 32 bit outlook in 64bit os issue

--- Practice/Win32/ApplicationUtils.pas
+++ Practice/Win32/ApplicationUtils.pas
@@ -19,11 +19,12 @@ function EnableMainForm: Boolean;
 procedure MakeMainFormForeground;
 function CanProcessCommands : boolean;
 function IsWoW64 : Boolean;
+function IsOutlook64Bit: Boolean;
 
 //******************************************************************************
 implementation
 uses
-  Forms, Windows;
+  Forms, Windows,Registry, SysUtils;
 
 var
   FDisableMainFormCount : integer = 0;
@@ -38,6 +39,56 @@ begin
   Inc( FDisableMainFormCount);
 end;
 
+{To make sure outlook used in clients machine is 64 bit or not.}
+function IsOutlook64Bit: Boolean;
+
+// need this constant since this is not declared in windows unit. Others are present in windows unit
+const
+  SCS_64BIT_BINARY = 6;
+var
+  Registry : TRegistry;
+  OutlookPath : string;
+  BinaryType: DWORD;
+begin
+  { Create the OLE Object }
+  Registry := TRegistry.Create;
+  Result := False;
+  try
+    // Try under current user
+    Registry.RootKey := HKEY_CURRENT_USER;
+    Registry.Access := KEY_READ;
+    OutlookPath := '';
+
+    if Registry.OpenKeyReadOnly('SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\') and
+      (Registry.KeyExists('outlook.exe')) and
+      (Registry.OpenKeyReadOnly('outlook.exe')) then
+      OutlookPath := Registry.ReadString('path')
+    else
+    begin
+      // Try under local machine
+      Registry.RootKey := HKEY_LOCAL_MACHINE;
+      Registry.Access := KEY_READ;
+
+      if Registry.OpenKeyReadOnly('SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\') and
+        (Registry.KeyExists('outlook.exe')) and
+        (Registry.OpenKeyReadOnly('outlook.exe')) then
+        OutlookPath := Registry.ReadString('path');
+    end;
+    if (Trim(OutlookPath) <> '') and GetBinaryType(Pchar(OutlookPath+'outlook.exe'), Binarytype) then
+    case BinaryType of
+      {SCS_32BIT_BINARY,
+      SCS_DOS_BINARY,
+      SCS_WOW_BINARY,
+      SCS_PIF_BINARY,
+      SCS_POSIX_BINARY,
+      SCS_OS216_BINARY : Result := False;}
+      SCS_64BIT_BINARY : Result := True;
+    end;
+  finally
+    FreeAndNil(Registry);
+  end;
+end;
+
 function IsWoW64 : Boolean;
 var
   IsWow64Process: function(hProcess: THandle; out Wow64Process: Bool): Bool; stdcall;
@@ -50,7 +101,7 @@ begin
   if Assigned(IsWow64Process) then
     Wow64Proc := IsWow64Process(GetCurrentProcess, Wow64Proc) and Wow64Proc;
 
-  Result := Wow64Proc;
+  Result := Wow64Proc and IsOutlook64Bit;
 end;
 
 function EnableMainForm: Boolean;
